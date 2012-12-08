aae9f6f1cb224cbf7e2dd3054c80f31d24e00cde
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2012-12-08 12:19:52 +0100

    KISSDecomposer: actually parse options

--- lib/art_decomp/kiss_decomposer.rb
+++ lib/art_decomp/kiss_decomposer.rb
@@ -1,3 +1,6 @@
+require 'optparse'
+require 'ostruct'
+
 module ArtDecomp class KISSDecomposer
   def initialize opts = {}
     @decomposer     = opts.fetch(:decomposer)     { Decomposer.new    }
@@ -6,14 +9,25 @@ module ArtDecomp class KISSDecomposer
   end
 
   def decompose args
-    _, vhdl_path, kiss_path = args
-    circuit    = kiss_parser.circuit_for File.read kiss_path
+    options    = options_from args
+    circuit    = kiss_parser.circuit_for File.read options.kiss_path
     decomposed = decomposer.decompose circuit
     vhdl       = vhdl_presenter.vhdl_for decomposed
-    name       = File.basename kiss_path, '.kiss'
-    File.write "#{vhdl_path}/#{name}.vhdl", vhdl
+    name       = File.basename options.kiss_path, '.kiss'
+    File.write "#{options.vhdl_path}/#{name}.vhdl", vhdl
   end
 
   attr_reader :decomposer, :kiss_parser, :vhdl_presenter
   private     :decomposer, :kiss_parser, :vhdl_presenter
+
+  private
+
+  def options_from args
+    options = OpenStruct.new
+    OptionParser.new do |opts|
+      opts.on('--dir DIR', String) { |dir| options.vhdl_path = dir }
+    end.parse! args
+    options.kiss_path = args.first
+    options
+  end
 end end
