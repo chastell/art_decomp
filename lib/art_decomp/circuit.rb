d64733bb4374f0ce1161f4f3cebd13ab907fcd06
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-01-13 15:12:20 +0100

    Circuit: s/mappings/encodings

--- lib/art_decomp/circuit.rb
+++ lib/art_decomp/circuit.rb
@@ -1,5 +1,5 @@
 module ArtDecomp class Circuit
-  attr_reader :functions, :mappings
+  attr_reader :functions, :encodings
 
   def self.from_fsm opts = {}
     inputs  = opts.fetch :inputs
@@ -8,13 +8,13 @@ module ArtDecomp class Circuit
     o_state = opts.fetch :o_state
     function_factory = opts.fetch(:function_factory) { Function }
     function = function_factory.new inputs.map(&:values) + [i_state.values], outputs.map(&:values) + [o_state.values]
-    new functions: [function], mappings: {
+    new functions: [function], encodings: {
       function => [inputs.map(&:keys) + [i_state.keys], outputs.map(&:keys) + [o_state.keys]]
     }
   end
 
   def initialize opts = {}
+    @encodings = opts.fetch(:encodings) { {} }
     @functions = opts.fetch(:functions) { [] }
-    @mappings  = opts.fetch(:mappings)  { {} }
   end
 end end
