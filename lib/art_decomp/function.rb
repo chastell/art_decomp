d5bb72914bdfa9c3c4c887458e64dd7a8d6c0f24
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-01-17 21:48:01 +0100

    second unhappy stabs at Function

--- lib/art_decomp/function.rb
+++ lib/art_decomp/function.rb
@@ -1,7 +1,8 @@
 module ArtDecomp class Function
-  attr_reader :table
+  attr_reader :encodings, :table
 
   def initialize inputs, outputs
-    @table = [inputs.map(&:values), outputs.map(&:values)]
+    @encodings = [inputs.map(&:keys),   outputs.map(&:keys)  ]
+    @table     = [inputs.map(&:values), outputs.map(&:values)]
   end
 end end
--- spec/art_decomp/function_spec.rb
+++ spec/art_decomp/function_spec.rb
@@ -1,20 +1,31 @@
 require_relative '../spec_helper'
 
 module ArtDecomp describe Function do
+  let(:inputs) {[
+    { :'0' => [0,2], :'1' => [1,2] },
+    { :'0' => [0,2], :'1' => [0,1] },
+    { s1: [0,1], s2: [], s3: [2]   },
+  ]}
+  let(:outputs) {[
+    { :'0' => [0,2],   :'1' => [1,2]   },
+    { :'0' => [0,1,2], :'1' => [0,1,2] },
+    { :'0' => [0,1,2], :'1' => [0,1]   },
+    { s1: [0,2], s2: [0,1], s3: [0]    },
+  ]}
+  let(:subject) { Function.new inputs, outputs }
+
+  describe '#encodings' do
+    it 'contains the encodings' do
+      subject.encodings.must_equal [
+        [[:'0', :'1'], [:'0', :'1'], [:s1, :s2, :s3]],
+        [[:'0', :'1'], [:'0', :'1'], [:'0', :'1'], [:s1, :s2, :s3]]
+      ]
+    end
+  end
+
   describe '#table' do
     it 'contains the truth table' do
-      inputs = [
-        { :'0' => [0,2], :'1' => [1,2] },
-        { :'0' => [0,2], :'1' => [0,1] },
-        { s1: [0,1], s2: [], s3: [2]   },
-      ]
-      outputs = [
-        { :'0' => [0,2],   :'1' => [1,2]   },
-        { :'0' => [0,1,2], :'1' => [0,1,2] },
-        { :'0' => [0,1,2], :'1' => [0,1]   },
-        { s1: [0,2], s2: [0,1], s3: [0]    },
-      ]
-      Function.new(inputs, outputs).table.must_equal [
+      subject.table.must_equal [
         [[[0,2], [1,2]], [[0,2], [0,1]], [[0,1], [], [2]]],
         [[[0,2], [1,2]], [[0,1,2], [0,1,2]], [[0,1,2], [0,1]], [[0,2], [0,1], [0]]]
       ]
