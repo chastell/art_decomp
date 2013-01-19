59e43357c69ab08af84ebe2d9b6b00b5cc36eef1
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-01-19 16:47:02 +0100

    simplify Circuit spec

--- spec/art_decomp/circuit_spec.rb
+++ spec/art_decomp/circuit_spec.rb
@@ -3,32 +3,13 @@ require_relative '../spec_helper'
 module ArtDecomp describe Circuit do
   describe '.from_fsm' do
     it 'creates a Circuit representing the FSM' do
-      inputs = [
-        { :'0' => [0,2], :'1' => [1,2] },
-        { :'0' => [0,2], :'1' => [0,1] },
-      ]
-      outputs = [
-        { :'0' => [0,2],   :'1' => [1,2]   },
-        { :'0' => [0,1,2], :'1' => [0,1,2] },
-        { :'0' => [0,1,2], :'1' => [0,1]   },
-      ]
-      i_state = { s1: [0,1], s2: [],    s3: [2] }
-      o_state = { s1: [0,2], s2: [0,1], s3: [0] }
+      inputs  = [{ :'0' => [0], :'1' => [1] }]
+      outputs = [{ :'0' => [1], :'1' => [0] }]
+      i_state = { s1: [0], s2: [1], s3: [2] }
+      o_state = { s1: [1], s2: [2], s3: [0] }
 
       function = Object.new
-      function_factory = MiniTest::Mock.new.expect :new, function, [
-        [
-          { :'0' => [0,2], :'1' => [1,2] },
-          { :'0' => [0,2], :'1' => [0,1] },
-          { s1: [0,1], s2: [], s3: [2]   },
-        ],
-        [
-          { :'0' => [0,2],   :'1' => [1,2]   },
-          { :'0' => [0,1,2], :'1' => [0,1,2] },
-          { :'0' => [0,1,2], :'1' => [0,1]   },
-          { s1: [0,2], s2: [0,1], s3: [0]    },
-        ],
-      ]
+      function_factory = MiniTest::Mock.new.expect :new, function, [inputs + [i_state], outputs + [o_state]]
 
       circuit = Circuit.from_fsm function_factory: function_factory,
         inputs: inputs, i_state: i_state, outputs: outputs, o_state: o_state
