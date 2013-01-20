4675c732ef56e0a5121efb29ec0721b74b1aeecf
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-01-20 19:31:06 +0100

    simplify Function spec

--- spec/art_decomp/function_spec.rb
+++ spec/art_decomp/function_spec.rb
@@ -1,24 +1,15 @@
 require_relative '../spec_helper'
 
 module ArtDecomp describe Function do
-  let(:inputs) {[
-    { :'0' => [0,2], :'1' => [1,2] },
-    { :'0' => [0,2], :'1' => [0,1] },
-    { s1: [0,1], s2: [], s3: [2]   },
-  ]}
-  let(:outputs) {[
-    { :'0' => [0,2],   :'1' => [1,2]   },
-    { :'0' => [0,1,2], :'1' => [0,1,2] },
-    { :'0' => [0,1,2], :'1' => [0,1]   },
-    { s1: [0,2], s2: [0,1], s3: [0]    },
-  ]}
+  let(:inputs)  { [{ :'0' => [0], :'1' => [1] }, { s1: [0], s2: [1], s3: [2] }] }
+  let(:outputs) { [{ :'0' => [1], :'1' => [0] }, { s1: [1], s2: [2], s3: [0] }] }
   let(:subject) { Function.new inputs, outputs }
 
   describe '#encodings' do
     it 'contains the encodings' do
       subject.encodings.must_equal [
-        [[:'0', :'1'], [:'0', :'1'], [:s1, :s2, :s3]],
-        [[:'0', :'1'], [:'0', :'1'], [:'0', :'1'], [:s1, :s2, :s3]]
+        [[:'0', :'1'], [:s1, :s2, :s3]],
+        [[:'0', :'1'], [:s1, :s2, :s3]],
       ]
     end
   end
@@ -26,8 +17,8 @@ module ArtDecomp describe Function do
   describe '#table' do
     it 'contains the truth table' do
       subject.table.must_equal [
-        [[[0,2], [1,2]], [[0,2], [0,1]], [[0,1], [], [2]]],
-        [[[0,2], [1,2]], [[0,1,2], [0,1,2]], [[0,1,2], [0,1]], [[0,2], [0,1], [0]]]
+        [[[0], [1]], [[0], [1], [2]]],
+        [[[1], [0]], [[1], [2], [0]]],
       ]
     end
   end
