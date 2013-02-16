5896c77d96ad66d3dbcbb5d944dd9a6e000db04c
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-02-16 20:21:39 +0100

    KISSDecomposer: reinject ALL the dependencies

--- lib/art_decomp/kiss_decomposer.rb
+++ lib/art_decomp/kiss_decomposer.rb
@@ -3,22 +3,23 @@ require 'ostruct'
 
 module ArtDecomp class KISSDecomposer
   def initialize opts = {}
-    @decomposer                = opts.fetch(:decomposer)                { Decomposer.new   }
-    @kiss_parser_factory       = opts.fetch(:kiss_parser_factory)       { KISSParser       }
-    @circuit_presenter_factory = opts.fetch(:circuit_presenter_factory) { CircuitPresenter }
+    @decomposer = opts.fetch(:decomposer) { Decomposer.new }
   end
 
-  def decompose args
-    options    = options_from args
-    circuit    = kiss_parser_factory.new(File.read options.kiss_path).circuit
-    decomposed = decomposer.decompose circuit
-    vhdl       = circuit_presenter_factory.new(decomposed).vhdl
-    name       = File.basename options.kiss_path, '.kiss'
+  def decompose args, opts = {}
+    options = options_from args
+    kiss    = File.read options.kiss_path
+    circuit = opts.fetch(:kiss_parser) { KISSParser.new kiss }.circuit
+    decd    = decomposer.decompose circuit
+
+    vhdl = opts.fetch(:circuit_presenter) { CircuitPresenter.new decd }.vhdl
+    name = File.basename options.kiss_path, '.kiss'
+
     File.write "#{options.vhdl_path}/#{name}.vhdl", vhdl
   end
 
-  attr_reader :circuit_presenter_factory, :decomposer, :kiss_parser_factory
-  private     :circuit_presenter_factory, :decomposer, :kiss_parser_factory
+  attr_reader :decomposer
+  private     :decomposer
 
   private
 
