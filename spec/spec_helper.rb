6d9db1297fed948c42beb1dc43201fd7809ff485
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-02-16 22:20:58 +0100

    specs: callable support in doubles

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,16 @@ class String
   end
 end
 
+class Double < OpenStruct
+  def initialize opts
+    callables, values = opts.partition { |_, val| val.is_a? Proc }
+    super values
+    callables.each do |name, callable|
+      define_singleton_method(name) { |*args| callable.call(*args) }
+    end
+  end
+end
+
 def double opts = {}
-  OpenStruct.new opts
+  Double.new opts
 end
