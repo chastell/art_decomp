7581856487b348fa9ab7ad86c0070a7754fb5906
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-02-16 20:30:40 +0100

    KISSDecomposer spec refactoring

--- spec/art_decomp/kiss_decomposer_spec.rb
+++ spec/art_decomp/kiss_decomposer_spec.rb
@@ -4,15 +4,16 @@ require 'tmpdir'
 module ArtDecomp describe KISSDecomposer do
   describe '#decompose' do
     it 'decomposes the given KISS file into VHDL implementation' do
-      circuit           = Object.new
-      decomposer        = MiniTest::Mock.new.expect :decompose, circuit, [circuit]
-      kiss_parser       = OpenStruct.new circuit: circuit
-      circuit_presenter = OpenStruct.new vhdl: 'some VHDL'
+      circuit    = Object.new
+      decomposer = MiniTest::Mock.new.expect :decompose, circuit, [circuit]
+      kp         = OpenStruct.new circuit: circuit
+      cp         = OpenStruct.new vhdl: 'some VHDL'
 
       Dir.mktmpdir do |vhdl_path|
         File.stub :read, 'some KISS' do
-          kiss_decomposer = KISSDecomposer.new ['--dir', vhdl_path, 'foo/bar/mc.kiss'], decomposer: decomposer
-          kiss_decomposer.decompose circuit_presenter: circuit_presenter, kiss_parser: kiss_parser
+          args = ['--dir', vhdl_path, 'foo/bar/mc.kiss']
+          kiss_decomposer = KISSDecomposer.new args, decomposer: decomposer
+          kiss_decomposer.decompose circuit_presenter: cp, kiss_parser: kp
         end
         File.read("#{vhdl_path}/mc.vhdl").must_equal 'some VHDL'
       end
