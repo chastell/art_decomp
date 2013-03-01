d87bd85fcc9dec37ffd63d43eb744c49a0c405ce
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-03-01 18:45:23 +0100

    factor out FunctionPresenter#mapping_for and #width_of

--- lib/art_decomp/function_presenter.rb
+++ lib/art_decomp/function_presenter.rb
@@ -21,23 +21,29 @@ module ArtDecomp class FunctionPresenter
   private
 
   def column_from put
-    all     = put.keys.sort
-    width   = Math.log2(all.size).ceil
-    mapping = Hash[all.map.with_index do |key, index|
-      [key, index.to_s(2).rjust(width, '0')]
-    end]
-    dont_care = DontCare.to_s * width
+    dont_care = DontCare.to_s * width_of(put)
     Array.new(put.values.flatten.max + 1) do |row|
-      entry_for put, row, mapping, dont_care
+      entry_for put, row, dont_care
     end
   end
 
-  def entry_for put, row, mapping, dont_care
+  def entry_for put, row, dont_care
+    mapping = mapping_for put
     keys = put.select { |code, rows| rows.include? row }.keys.sort
     case
     when keys == put.keys.sort then dont_care
     when keys.size == 1        then mapping[keys.first]
     else                       raise 'trying to map multiple (but not all) keys'
     end
+  end
+
+  def mapping_for put
+    Hash[put.keys.sort.map.with_index do |key, index|
+      [key, index.to_s(2).rjust(width_of(put), '0')]
+    end]
+  end
+
+  def width_of put
+    Math.log2(put.size).ceil
   end
 end end
