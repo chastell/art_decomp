169a4c66337545443dcf26e0976d3fe13fc0f9e8
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-03-16 13:02:02 +0100

    KISSDecomposer: pass --width to Decomposer

--- lib/art_decomp/kiss_decomposer.rb
+++ lib/art_decomp/kiss_decomposer.rb
@@ -9,7 +9,7 @@ module ArtDecomp class KISSDecomposer
   def decompose(circuit_provider: KISSParser, decomposer: Decomposer, vhdl_provider: CircuitPresenter)
     kiss = File.read settings.kiss_path
     circ = circuit_provider.circuit_from_kiss kiss
-    decd = decomposer.decompose_circuit circ
+    decd = decomposer.decompose_circuit circ, width: settings.width
     name = File.basename settings.kiss_path, '.kiss'
     vhdl = vhdl_provider.vhdl_for_circuit decd, name
     File.write "#{settings.vhdl_path}/#{name}.vhdl", vhdl
@@ -23,7 +23,8 @@ module ArtDecomp class KISSDecomposer
   def settings_from args
     OpenStruct.new.tap do |settings|
       OptionParser.new do |opts|
-        opts.on('--dir DIR', String) { |dir| settings.vhdl_path = dir }
+        opts.on('--dir DIR',     String)  { |dir|   settings.vhdl_path = dir }
+        opts.on('--width WIDTH', Integer) { |width| settings.width = width   }
       end.parse! args
       settings.kiss_path = args.first
     end
