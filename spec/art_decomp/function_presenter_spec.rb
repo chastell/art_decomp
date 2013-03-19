8f77f5e0a718e1013c130758f133a0ba9c63e1f2
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-03-19 10:16:03 +0100

    specs: desubjectify

--- spec/art_decomp/function_presenter_spec.rb
+++ spec/art_decomp/function_presenter_spec.rb
@@ -15,11 +15,11 @@ module ArtDecomp describe FunctionPresenter do
     { :'0' => [0, 1, 2, 3, 4, 5, 6, 7], :'1' => [8, 9] },
     { :HG => [0, 1, 9], :HY => [2, 3], :FG => [4, 5], :FY => [6, 7, 8] },
   ]}
-  let(:subject) { FunctionPresenter.new Function.new(is, os) }
+  let(:function_presenter) { FunctionPresenter.new Function.new(is, os) }
 
   describe '#rows' do
     it 'returns binary-encoded row representation of the function' do
-      subject.rows.must_equal [
+      function_presenter.rows.must_equal [
         ['0--10', '0001010'],
         ['-0-10', '0001010'],
         ['11-10', '1001011'],
@@ -36,8 +36,8 @@ module ArtDecomp describe FunctionPresenter do
 
   describe '#widths' do
     it 'returns the Function widths' do
-      subject.widths(:i).must_equal [1, 1, 1, 2]
-      subject.widths(:o).must_equal [1, 1, 1, 1, 1, 2]
+      function_presenter.widths(:i).must_equal [1, 1, 1, 2]
+      function_presenter.widths(:o).must_equal [1, 1, 1, 1, 1, 2]
     end
   end
 end end
--- spec/art_decomp/function_spec.rb
+++ spec/art_decomp/function_spec.rb
@@ -3,31 +3,31 @@ require_relative '../spec_helper'
 module ArtDecomp describe Function do
   let(:is) { [{ :'0' => [0], :'1' => [1] }, { s1: [0], s2: [1], s3: [2] }] }
   let(:os) { [{ :'0' => [1], :'1' => [0] }, { s1: [1], s2: [2], s3: [0] }] }
-  let(:subject) { Function.new is, os }
+  let(:function) { Function.new is, os }
 
   describe '#is' do
     it 'returns the input signals' do
-      subject.is.must_equal is
+      function.is.must_equal is
     end
   end
 
   describe '#os' do
     it 'returns the input signals' do
-      subject.os.must_equal os
+      function.os.must_equal os
     end
   end
 
   describe '#width' do
     it 'returns binary width of the function' do
       Function.new([], []).width.must_equal 0
-      subject.width.must_equal 3
+      function.width.must_equal 3
     end
   end
 
   describe '#widths' do
     it 'returns binary widths of signals' do
-      subject.widths(:i).must_equal [1, 2]
-      subject.widths(:o).must_equal [1, 2]
+      function.widths(:i).must_equal [1, 2]
+      function.widths(:o).must_equal [1, 2]
     end
   end
 end end
