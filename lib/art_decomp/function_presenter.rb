26c58acfa1a41ef34a94541fbd2d631a5633fe40
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-04-24 10:29:50 +0200

    FunctionPresenter: smplfy

--- lib/art_decomp/function_presenter.rb
+++ lib/art_decomp/function_presenter.rb
@@ -10,22 +10,19 @@ module ArtDecomp class FunctionPresenter < SimpleDelegator
   private
 
   def column_from put
-    dc_line = DontCare.to_s * ArtDecomp.width_of(put)
-    Array.new(put.values.max.to_s(2).size) do |row|
-      entry_for put, row, dc_line
-    end
+    Array.new(put.values.max.to_s(2).size) { |row| entry_for put, row }
   end
 
   def columns_from puts
     puts.map { |put| column_from put }.transpose.map(&:join)
   end
 
-  def entry_for put, row, dc_line
+  def entry_for put, row
     keys = put.select { |code, bits| (bits & 1 << row).nonzero? }.keys.sort
     case keys.size
-    when put.keys.size then dc_line
-    when 1             then mapping_for put, keys.first
-    else               raise 'trying to map multiple (but not all) keys'
+    when put.size then DontCare.to_s * ArtDecomp.width_of(put)
+    when 1        then mapping_for put, keys.first
+    else          raise 'trying to map multiple (but not all) keys'
     end
   end
 
