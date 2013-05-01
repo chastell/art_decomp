df43ae0364837eb1012f2e145179a6d69baec9bc
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-05-01 09:16:11 +0200

    Put spec: factor out a common subject

--- spec/art_decomp/put_spec.rb
+++ spec/art_decomp/put_spec.rb
@@ -1,28 +1,29 @@
 require_relative '../spec_helper'
 
 module ArtDecomp describe Put do
+  let(:put) { Put[a: B[0,1], b: B[1,2]] }
+
   describe '.[]' do
     it 'creates a new Put with the given blanket' do
-      Put[a: B[0,1], b: B[1,2]]
-        .must_equal Put.new blanket: { a: B[0,1], b: B[1,2] }
+      put.must_equal Put.new blanket: { a: B[0,1], b: B[1,2] }
     end
   end
 
   describe '#blocks' do
     it 'returns the blanket’s blocks' do
-      Put[a: B[0,1], b: B[1,2]].blocks.must_equal [B[0,1], B[1,2]]
+      put.blocks.must_equal [B[0,1], B[1,2]]
     end
   end
 
   describe '#codes' do
     it 'returns the blanket’s codes' do
-      Put[a: B[0,1], b: B[1,2]].codes.must_equal [:a, :b]
+      put.codes.must_equal [:a, :b]
     end
   end
 
   describe '#size' do
     it 'returns the size of the blanket' do
-      Put[a: B[0,1], b: B[1,2]].size.must_equal 2
+      put.size.must_equal 2
     end
   end
 end end
