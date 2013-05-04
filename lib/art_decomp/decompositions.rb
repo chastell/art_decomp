8938a87b84969b756f8078554c6b65f2d0e35e98
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-05-04 09:29:41 +0200

    first cut of Decompositions#decompositions

--- lib/art_decomp/decompositions.rb
+++ lib/art_decomp/decompositions.rb
@@ -2,4 +2,23 @@ module ArtDecomp class Decompositions
   def self.for circuit
     new(circuit).decompositions
   end
+
+  def initialize circuit, decomposer: nil
+    @decomposer = decomposer
+    @queue      = [circuit]
+  end
+
+  def decompositions
+    Enumerator.new do |yielder|
+      until queue.empty?
+        circuit = queue.shift
+        yielder << circuit
+        decomposer.decomposed(circuit).each { |circ| queue << circ }
+        queue.sort_by!(&:not_smaller_than)
+      end
+    end
+  end
+
+  attr_reader :decomposer, :queue
+  private     :decomposer, :queue
 end end
--- spec/art_decomp/decompositions_spec.rb
+++ spec/art_decomp/decompositions_spec.rb
@@ -11,4 +11,22 @@ module ArtDecomp describe Decompositions do
       end
     end
   end
+
+  describe '#decompositions' do
+    it 'yields subsequent best decompositions' do
+      c1   = double not_smaller_than: 7
+      c11  = double not_smaller_than: 9
+      c111 = double not_smaller_than: 12
+      c112 = double not_smaller_than: 10
+      c12  = double not_smaller_than: 8
+      c121 = double not_smaller_than: 13
+      c13  = double not_smaller_than: 11
+      tree = { c1 => [c11, c12, c13], c11 => [c111, c112], c12 => [c121] }
+      tree.default = []
+      decomposer = double decomposed: ->(circuit) { tree[circuit].to_enum }
+      decs = Decompositions.new(c1, decomposer: decomposer).decompositions
+      decs.must_be_kind_of Enumerator
+      decs.to_a.must_equal [c1, c12, c11, c112, c13, c111, c121]
+    end
+  end
 end end
