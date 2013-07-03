e9a2e524df9f95bf7cde6859714c611803f8951f
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-07-03 09:51:43 +0200

    clean-up CircuitDecomposer spec

--- spec/art_decomp/circuit_decomposer_spec.rb
+++ spec/art_decomp/circuit_decomposer_spec.rb
@@ -2,15 +2,15 @@ require_relative '../spec_helper'
 
 module ArtDecomp describe CircuitDecomposer do
   describe '.decompose' do
-    it 'yields subsequent decomposed circuits' do
+    it 'yields subsequent decomposed Circuits' do
       circuit = fake :circuit, largest_function: fun = fake(:function)
-      f1, f2  = fake(:function), fake(:function)
+      c1, c2  = fake(:circuit), fake(:circuit)
       fun_dec = fake :function_decomposer
-      mock(fun_dec).decompose(fun) { [f1, f2].to_enum }
+      mock(fun_dec).decompose(fun) { [c1, c2] }
       d1, d2  = fake(:circuit), fake(:circuit)
       solder  = fake :circuit_solder
-      mock(solder).replace(circuit, fun, f1) { d1 }
-      mock(solder).replace(circuit, fun, f2) { d2 }
+      mock(solder).replace(circuit, fun, c1) { d1 }
+      mock(solder).replace(circuit, fun, c2) { d2 }
       decs = CircuitDecomposer.decompose circuit, decomposer: fun_dec,
         solder: solder
       decs.to_a.must_equal [d1, d2]
