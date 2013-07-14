d70609e28bb64df9f2ab90c8a6c3e1306f50d8af
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-07-14 22:37:19 +0200

    add CircuitSizer#min_size

--- lib/art_decomp/circuit_sizer.rb
+++ lib/art_decomp/circuit_sizer.rb
@@ -1,4 +1,8 @@
 module ArtDecomp class CircuitSizer
+  def initialize circuit
+    @circuit = circuit
+  end
+
   def adm_size
   end
 
@@ -6,5 +10,22 @@ module ArtDecomp class CircuitSizer
   end
 
   def min_size
+    (circuit.functions.map(&:arch).map do |arch|
+      min_quarters arch
+    end.reduce(0, :+) / 4.0).ceil
+  end
+
+  attr_reader :circuit
+  private     :circuit
+
+  private
+
+  def min_quarters arch
+    case
+    when arch.i == 0 then 0
+    when arch.o == 0 then 0
+    when arch.i <= 5 then (arch.o / 2.0).ceil
+    else [(arch.i / 5.0).ceil, (arch.o / 2.0).ceil].max
+    end
   end
 end end
--- spec/art_decomp/circuit_sizer_spec.rb
+++ spec/art_decomp/circuit_sizer_spec.rb
@@ -1,4 +1,25 @@
 require_relative '../spec_helper'
 
 module ArtDecomp describe CircuitSizer do
+  describe '#min_size' do
+    it 'returns the smallest possible size for the given Circuit' do
+      {
+        []                                => 0,
+        [Arch[0,0]]                       => 0,
+        [Arch[0,1]]                       => 0,
+        [Arch[1,0]]                       => 0,
+        [Arch[1,1]]                       => 1,
+        [Arch[5,2]]                       => 1,
+        [Arch[5,3], Arch[5,3], Arch[5,1]] => 2,
+        [Arch[7,0]]                       => 0,
+        [Arch[20,8]]                      => 1,
+        [Arch[21,8]]                      => 2,
+        [Arch[20,9]]                      => 2,
+      }.each do |archs, size|
+        functions = archs.map { |arch| fake :function, arch: arch }
+        circuit   = fake :circuit, :functions => functions
+        CircuitSizer.new(circuit).min_size.must_equal size
+      end
+    end
+  end
 end end
