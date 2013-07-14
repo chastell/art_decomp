6f4fc2088bc4e311ced041f3b1aac30207783eb8
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-07-14 22:38:58 +0200

    add CircuitSizer#max_size

--- lib/art_decomp/circuit_sizer.rb
+++ lib/art_decomp/circuit_sizer.rb
@@ -7,6 +7,9 @@ module ArtDecomp class CircuitSizer
   end
 
   def max_size
+    (circuit.functions.map(&:arch).map do |arch|
+      max_quarters arch
+    end.reduce(0, :+) / 4.0).ceil
   end
 
   def min_size
@@ -19,6 +22,17 @@ module ArtDecomp class CircuitSizer
   private     :circuit
 
   private
+
+  def max_quarters arch
+    case
+    when arch.i == 0 then 0
+    when arch.i <= 5 then (arch.o / 2.0).ceil
+    when arch.i == 6 then arch.o
+    when arch.i == 7 then arch.o * 2
+    when arch.i == 8 then arch.o * 4
+    else arch.o * (max_quarters(Arch[6,1]) + 4 * max_quarters(Arch[arch.i-2,1]))
+    end
+  end
 
   def min_quarters arch
     case
--- spec/art_decomp/circuit_sizer_spec.rb
+++ spec/art_decomp/circuit_sizer_spec.rb
@@ -1,6 +1,36 @@
 require_relative '../spec_helper'
 
 module ArtDecomp describe CircuitSizer do
+  describe '#max_size' do
+    it 'returns the maximum size for the given Circuit' do
+      {
+        []                                => 0,
+        [Arch[0,0]]                       => 0,
+        [Arch[0,1]]                       => 0,
+        [Arch[1,1]]                       => 1,
+        [Arch[5,2]]                       => 1,
+        [Arch[5,3], Arch[5,3], Arch[5,1]] => 2,
+        [Arch[5,8]]                       => 1,
+        [Arch[5,9]]                       => 2,
+        [Arch[6,4]]                       => 1,
+        [Arch[6,5]]                       => 2,
+        [Arch[7,0]]                       => 0,
+        [Arch[7,1], Arch[6,1]]            => 1,
+        [Arch[7,2]]                       => 1,
+        [Arch[7,3]]                       => 2,
+        [Arch[8,1]]                       => 1,
+        [Arch[8,2]]                       => 2,
+        [Arch[9,1]]                       => 3,
+        [Arch[9,4]]                       => 9,
+        [Arch[14,7]]                      => 485,
+      }.each do |archs, size|
+        functions = archs.map { |arch| fake :function, arch: arch }
+        circuit   = fake :circuit, :functions => functions
+        CircuitSizer.new(circuit).max_size.must_equal size
+      end
+    end
+  end
+
   describe '#min_size' do
     it 'returns the smallest possible size for the given Circuit' do
       {
