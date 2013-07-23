5fec3c2f17cf19afe951ac72c614560d7ee7aa39
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-07-23 14:37:50 +0200

    pending FunctionSimplifier#simplify spec

---
+++ spec/art_decomp/function_simplifier_spec.rb
@@ -0,0 +1,7 @@
+require_relative '../spec_helper'
+
+module ArtDecomp describe FunctionSimplifier do
+  describe '#simplify' do
+    it 'returns the simplest implementation of a Function'
+  end
+end end
