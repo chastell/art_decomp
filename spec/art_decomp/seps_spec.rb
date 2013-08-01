966d456108daaaf365ebd422122712990aa41ba1
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-08-01 23:10:44 +0200

    Seps spec: deglorify the example

--- spec/art_decomp/seps_spec.rb
+++ spec/art_decomp/seps_spec.rb
@@ -1,18 +1,16 @@
 require_relative '../spec_helper'
 
 module ArtDecomp describe Seps do
-  let(:seps) { Seps[B[0,1], B[1,2]] }
-
   describe '.[]' do
     it 'creates Seps from the given blocks' do
-      seps.must_equal Seps.new(blocks: [B[0,1], B[1,2]])
+      Seps[B[0,1], B[1,2]].must_equal Seps.new(blocks: [B[0,1], B[1,2]])
       Seps[].must_equal Seps.new
     end
   end
 
   describe '.new' do
     it 'can take a matrix to start from' do
-      Seps.new(matrix: [0b100, 0b000, 0b001]).must_equal seps
+      Seps.new(matrix: [0b100, 0b000, 0b001]).must_equal Seps[B[0,1], B[1,2]]
     end
 
     it 'normalises the matrix' do
@@ -30,9 +28,9 @@ module ArtDecomp describe Seps do
 
   describe '#==' do
     it 'compares two Seps by value' do
-      assert seps == seps.dup
-      assert seps == Seps[B[1,2], B[0,1]]
-      refute seps == Seps[B[0,2], B[1,2]]
+      assert Seps[B[0,1], B[1,2]] == Seps[B[0,1], B[1,2]].dup
+      assert Seps[B[0,1], B[1,2]] == Seps[B[1,2], B[0,1]]
+      refute Seps[B[0,1], B[1,2]] == Seps[B[0,2], B[1,2]]
     end
   end
 end end
