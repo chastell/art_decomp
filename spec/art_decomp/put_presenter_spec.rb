41ab1b31bc8e3b77c059c339b183175a6faf1695
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-10-05 20:42:43 +0200

    add PutPresenter#bin_column

--- spec/art_decomp/put_presenter_spec.rb
+++ spec/art_decomp/put_presenter_spec.rb
@@ -1,6 +1,15 @@
 require_relative '../spec_helper'
 
 module ArtDecomp describe PutPresenter do
+  describe '#bin_column' do
+    it 'returns the binary column representing the Put' do
+      PutPresenter.new(Put[a: B[0,1], b: B[1,2]]).bin_column
+        .must_equal %w[0 - 1]
+      PutPresenter.new(Put[a: B[0], b: B[1], c: B[2]]).bin_column
+        .must_equal %w[00 01 10]
+    end
+  end
+
   describe '#mapping_for' do
     it 'returns a String version of the binary mapping for a given code' do
       put_presenter = PutPresenter.new Put[a: B[0,1], b: B[1,2]]
