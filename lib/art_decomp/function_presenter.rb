7b826e5f9310c7a3c43ce50a25200d62760b51f5
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-10-10 10:32:30 +0200

    move/privatise PutPresenter into FunctionPresenter

--- lib/art_decomp/function_presenter.rb
+++ lib/art_decomp/function_presenter.rb
@@ -12,4 +12,25 @@ module ArtDecomp class FunctionPresenter < SimpleDelegator
   def columns_from puts
     puts.map { |put| PutPresenter.new(put).bin_column }.transpose.map(&:join)
   end
+
+  class PutPresenter < SimpleDelegator
+    DontCare = :-
+
+    def bin_column
+      Array.new(blocks.max.to_s(2).size) { |row| entry_for row }
+    end
+
+    def entry_for row
+      row_codes = codes { |code, block| (block & B[row]).nonzero? }.sort
+      case row_codes.size
+      when size then DontCare.to_s * binwidth
+      when 1    then mapping_for row_codes.first
+      else      raise 'trying to map multiple (but not all) codes'
+      end
+    end
+
+    def mapping_for code
+      codes.sort.index(code).to_s(2).rjust binwidth, '0'
+    end
+  end
 end end
