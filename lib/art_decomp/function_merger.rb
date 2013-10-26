dde62528569cebd0ed747d24445379e479f5f5be
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-10-26 21:10:20 +0200

    FunctionMerger#merge: optimise result

--- lib/art_decomp/function_merger.rb
+++ lib/art_decomp/function_merger.rb
@@ -1,7 +1,7 @@
 module ArtDecomp class FunctionMerger
   def merge functions
     functions.group_by(&:is).map do |is, funs|
-      Function.new Puts.new is: is, os: funs.flat_map(&:os)
+      Function.new Puts.new is: is, os: funs.flat_map(&:os).uniq
     end
   end
 end end
