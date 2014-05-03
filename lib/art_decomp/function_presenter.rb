c02773f98d9ef51f963d1e0e6614e95642512c02
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2014-05-03 19:58:43 +0200

    rename PutGroupPresenter to PutsPresenter

--- lib/art_decomp/function_presenter.rb
+++ lib/art_decomp/function_presenter.rb
@@ -1,10 +1,10 @@
 require 'delegate'
-require_relative 'put_group_presenter'
+require_relative 'puts_presenter'
 
 module ArtDecomp class FunctionPresenter < SimpleDelegator
   def rows
-    is_cols = PutGroupPresenter.new(is).bin_columns
-    os_cols = PutGroupPresenter.new(os).bin_columns
+    is_cols = PutsPresenter.new(is).bin_columns
+    os_cols = PutsPresenter.new(os).bin_columns
     is_cols.zip os_cols
   end
 end end
--- lib/art_decomp/puts_presenter.rb
+++ lib/art_decomp/puts_presenter.rb
@@ -1,7 +1,7 @@
 require 'delegate'
 require_relative 'put_presenter'
 
-module ArtDecomp class PutGroupPresenter < SimpleDelegator
+module ArtDecomp class PutsPresenter < SimpleDelegator
   def bin_columns
     map { |put| PutPresenter.new(put).bin_column }.transpose.map(&:join)
   end
