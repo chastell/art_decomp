5ea93e6a6617be1f1ec39a3b64060181bf9b109f
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2014-05-03 19:07:52 +0200

    move CircuitSizer::ArchSizer to its own file

--- lib/art_decomp/circuit_sizer.rb
+++ lib/art_decomp/circuit_sizer.rb
@@ -1,5 +1,4 @@
-require 'delegate'
-require_relative 'arch'
+require_relative 'circuit_sizer/arch_sizer'
 
 module ArtDecomp class CircuitSizer
   def initialize circuit
@@ -29,31 +28,5 @@ module ArtDecomp class CircuitSizer
 
   def min_for_larger
     archs.reject(&:fits?).map(&:min_quarters)
-  end
-
-  class ArchSizer < SimpleDelegator
-    def fits?
-      i <= 8
-    end
-
-    def max_quarters
-      case
-      when i.zero?, o.zero? then 0
-      when i <= 5           then (o / 2.0).ceil
-      when i == 6           then o
-      when i == 7           then o * 2
-      when i == 8           then o * 4
-      else
-        o * (1 + 4 * self.class.new(Arch[i - 2, 1]).max_quarters)
-      end
-    end
-
-    def min_quarters
-      case
-      when i.zero?, o.zero? then 0
-      when i <= 5           then (o / 2.0).ceil
-      else [(i / 5.0).ceil, (o / 2.0).ceil].max
-      end
-    end
   end
 end end
---
+++ lib/art_decomp/circuit_sizer/arch_sizer.rb
@@ -0,0 +1,28 @@
+require 'delegate'
+require_relative '../arch'
+
+module ArtDecomp class CircuitSizer; class ArchSizer < SimpleDelegator
+  def fits?
+    i <= 8
+  end
+
+  def max_quarters
+    case
+    when i.zero?, o.zero? then 0
+    when i <= 5           then (o / 2.0).ceil
+    when i == 6           then o
+    when i == 7           then o * 2
+    when i == 8           then o * 4
+    else
+      o * (1 + 4 * self.class.new(Arch[i - 2, 1]).max_quarters)
+    end
+  end
+
+  def min_quarters
+    case
+    when i.zero?, o.zero? then 0
+    when i <= 5           then (o / 2.0).ceil
+    else [(i / 5.0).ceil, (o / 2.0).ceil].max
+    end
+  end
+end end end
