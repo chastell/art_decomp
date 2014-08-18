03f4430afacfbfbd5fccd0004e91cbbbef1d0423
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2014-08-18 13:13:58 +0200

    ArchSizer#max_quarters: minor change

--- lib/art_decomp/arch_sizer.rb
+++ lib/art_decomp/arch_sizer.rb
@@ -14,8 +14,7 @@ module ArtDecomp
       when i == 6           then o
       when i == 7           then o * 2
       when i == 8           then o * 4
-      else
-        o * (1 + 4 * self.class.new(Arch[i - 2, 1]).max_quarters)
+      else o * (1 + 4 * self.class.new(Arch[i - 2, 1]).max_quarters)
       end
     end
 
