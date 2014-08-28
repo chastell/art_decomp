ee22e07c43ebc43acb07f675bc5f136559984cf7
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2014-08-28 07:41:32 +0200

    spec Settings#vhdl_path

--- spec/art_decomp/settings_spec.rb
+++ spec/art_decomp/settings_spec.rb
@@ -1,3 +1,4 @@
+require 'tmpdir'
 require_relative '../spec_helper'
 require_relative '../../lib/art_decomp/settings'
 
@@ -6,6 +7,14 @@ module ArtDecomp
     describe '#kiss_path' do
       it 'is set by the first argument' do
         Settings.new(%w(foo/bar/mc.kiss)).kiss_path.must_equal 'foo/bar/mc.kiss'
+      end
+    end
+
+    describe '#vhdl_path' do
+      it 'is parsed from --dir' do
+        Dir.mktmpdir do |vhdl_path|
+          Settings.new(%W(--dir #{vhdl_path})).vhdl_path.must_equal vhdl_path
+        end
       end
     end
   end
