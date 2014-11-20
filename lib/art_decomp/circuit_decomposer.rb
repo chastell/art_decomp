6b5cc2fd7cd21284938e9ad4b3ed61248f4e75b7
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2014-11-20 06:02:13 +0100

    alphabetise all the kwargs

--- lib/art_decomp/circuit_decomposer.rb
+++ lib/art_decomp/circuit_decomposer.rb
@@ -3,8 +3,8 @@ require_relative 'function_decomposer'
 
 module ArtDecomp
   module CircuitDecomposer
-    def self.decompose(circuit, function_decomposer: FunctionDecomposer,
-                       circuit_solder: CircuitSolder.new)
+    def self.decompose(circuit, circuit_solder: CircuitSolder.new,
+                                function_decomposer: FunctionDecomposer)
       function = circuit.largest_function
       Enumerator.new do |yielder|
         function_decomposer.decompose(function).each do |decomposed|
