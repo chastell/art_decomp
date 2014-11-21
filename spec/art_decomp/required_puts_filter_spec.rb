c202a5da704635ebec97de9440df9950ac6f7c77
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2014-11-21 21:14:38 +0100

    RequiredPutsFilter: spec a static edge case

--- spec/art_decomp/required_puts_filter_spec.rb
+++ spec/art_decomp/required_puts_filter_spec.rb
@@ -7,24 +7,26 @@ module ArtDecomp
   describe RequiredPutsFilter do
     describe '.required' do
       it 'returns puts required by the given seps' do
-        #   | a b c | anb buc
-        # --+-------+--------
-        # 0 | 0 0 0 |  0   0
-        # 1 | 0 0 1 |  0   1
-        # 2 | 0 1 0 |  0   1
-        # 3 | 0 1 1 |  0   1
-        # 4 | 1 0 0 |  0   0
-        # 5 | 1 0 1 |  0   1
-        # 6 | 1 1 0 |  1   1
-        # 7 | 1 1 1 |  1   1
-        a    = Put[:'0' => B[0,1,2,3], :'1' => B[4,5,6,7]]
-        b    = Put[:'0' => B[0,1,4,5], :'1' => B[2,3,6,7]]
-        c    = Put[:'0' => B[0,2,4,6], :'1' => B[1,3,5,7]]
-        anb  = Put[:'0' => B[0,1,2,3,4,5], :'1' => B[6,7]]
-        buc  = Put[:'0' => B[0,4], :'1' => B[1,2,3,5,6,7]]
-        rpf  = RequiredPutsFilter
+        #   | a b c | anb buc one
+        # --+-------+------------
+        # 0 | 0 0 0 |  0   0   1
+        # 1 | 0 0 1 |  0   1   1
+        # 2 | 0 1 0 |  0   1   1
+        # 3 | 0 1 1 |  0   1   1
+        # 4 | 1 0 0 |  0   0   1
+        # 5 | 1 0 1 |  0   1   1
+        # 6 | 1 1 0 |  1   1   1
+        # 7 | 1 1 1 |  1   1   1
+        a   = Put[:'0' => B[0,1,2,3], :'1' => B[4,5,6,7]]
+        b   = Put[:'0' => B[0,1,4,5], :'1' => B[2,3,6,7]]
+        c   = Put[:'0' => B[0,2,4,6], :'1' => B[1,3,5,7]]
+        anb = Put[:'0' => B[0,1,2,3,4,5], :'1' => B[6,7]]
+        buc = Put[:'0' => B[0,4], :'1' => B[1,2,3,5,6,7]]
+        one = Put[:'0' => B[0,1,2,3,4,5,6,7]]
+        rpf = RequiredPutsFilter
         rpf.required(puts: [a, b, c], seps: anb.seps).must_equal [a, b]
         rpf.required(puts: [a, b, c], seps: buc.seps).must_equal [c, b]
+        rpf.required(puts: [a, b, c], seps: one.seps).must_be_empty
       end
     end
   end
