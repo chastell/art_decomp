3ddd1bf6da09b66441215f7f05baa4942a7fc26a
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2014-11-21 20:46:39 +0100

    FunctionSimplifier spec: better naming

--- spec/art_decomp/function_simplifier_spec.rb
+++ spec/art_decomp/function_simplifier_spec.rb
@@ -19,18 +19,17 @@ module ArtDecomp
         # 5 | 1 0 1 |  0   1
         # 6 | 1 1 0 |  1   1
         # 7 | 1 1 1 |  1   1
-        a    = Put[:'0' => B[0,1,2,3], :'1' => B[4,5,6,7]]
-        b    = Put[:'0' => B[0,1,4,5], :'1' => B[2,3,6,7]]
-        c    = Put[:'0' => B[0,2,4,6], :'1' => B[1,3,5,7]]
-        anb  = Put[:'0' => B[0,1,2,3,4,5], :'1' => B[6,7]]
-        buc  = Put[:'0' => B[0,4], :'1' => B[1,2,3,5,6,7]]
-        fanb = Function.new(Puts.new(is: [a, b, c], os: [anb]))
-        fbuc = Function.new(Puts.new(is: [a, b, c], os: [buc]))
-        fs   = FunctionSimplifier
-        fab_anb = Function.new(Puts.new(is: [a,b], os: [anb]))
-        fab_buc = Function.new(Puts.new(is: [c,b], os: [buc]))
-        fs.simplify(fanb).must_equal fab_anb
-        fs.simplify(fbuc).must_equal fab_buc
+        a   = Put[:'0' => B[0,1,2,3], :'1' => B[4,5,6,7]]
+        b   = Put[:'0' => B[0,1,4,5], :'1' => B[2,3,6,7]]
+        c   = Put[:'0' => B[0,2,4,6], :'1' => B[1,3,5,7]]
+        anb = Put[:'0' => B[0,1,2,3,4,5], :'1' => B[6,7]]
+        buc = Put[:'0' => B[0,4], :'1' => B[1,2,3,5,6,7]]
+        ab_anb  = Function.new(Puts.new(is: [a,b],   os: [anb]))
+        abc_anb = Function.new(Puts.new(is: [a,b,c], os: [anb]))
+        abc_buc = Function.new(Puts.new(is: [a,b,c], os: [buc]))
+        cb_buc  = Function.new(Puts.new(is: [c,b],   os: [buc]))
+        FunctionSimplifier.simplify(abc_anb).must_equal ab_anb
+        FunctionSimplifier.simplify(abc_buc).must_equal cb_buc
       end
     end
   end
