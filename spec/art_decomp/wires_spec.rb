c501db4ec48b46fee779024b4f0c828efa70b076
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2014-11-27 00:28:26 +0100

    spec Wires#==

--- spec/art_decomp/wires_spec.rb
+++ spec/art_decomp/wires_spec.rb
@@ -6,15 +6,33 @@ require_relative '../../lib/art_decomp/wires'
 
 module ArtDecomp
   describe Wires do
+    let(:fun_a) { fake(:function) }
+    let(:fun_b) { fake(:function) }
+    let(:wires) do
+      Wires.new([
+        Wire[Pin[fun_a, :os, 0], Pin[fun_b, :is, 1]],
+        Wire[Pin[fun_a, :os, 1], Pin[fun_b, :is, 0]],
+      ])
+    end
+
     describe '.from_hash' do
       it 'constructs the Wires from a minimal Hash' do
-        fun_a, fun_b = fake(:function), fake(:function)
         Wires.from_hash(
           [fun_a, :os, 0] => [fun_b, :is, 1],
           [fun_a, :os, 1] => [fun_b, :is, 0],
-        ).must_equal Wires.new([
+        ).must_equal wires
+      end
+    end
+
+    describe '#==' do
+      it 'compares two Wires with regard to contents' do
+        wires.must_equal Wires.new([
           Wire[Pin[fun_a, :os, 0], Pin[fun_b, :is, 1]],
           Wire[Pin[fun_a, :os, 1], Pin[fun_b, :is, 0]],
+        ])
+        wires.wont_equal Wires.new([
+          Wire[Pin[fun_a, :os, 0], Pin[fun_b, :is, 0]],
+          Wire[Pin[fun_a, :os, 1], Pin[fun_b, :is, 1]],
         ])
       end
     end
