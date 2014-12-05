84b241fa86259758af940fe85c590e8d7d5899c1
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2014-12-05 21:02:11 +0100

    Wire(s)Presenter: keep circuit knowledge higher

--- lib/art_decomp/wire_presenter.rb
+++ lib/art_decomp/wire_presenter.rb
@@ -1,43 +1,22 @@
 require 'delegate'
-require 'forwardable'
 
 module ArtDecomp
   class WirePresenter < SimpleDelegator
-    extend Forwardable
-
-    def initialize(wire, circuit:)
-      super wire
-      @circuit = circuit
-    end
-
     def wirings
       Array.new(dst.object.binwidths(dst.group)[dst.index]) do |n|
         wiring_for(n)
       end
     end
 
-    attr_reader :circuit
-    private     :circuit
-
     private
-
-    delegate %i(functions recoders) => :circuit
 
     def wiring_for(n)
       src_bin = src.object.binwidths(src.group)[0...src.index].reduce(0, :+) + n
       dst_bin = dst.object.binwidths(dst.group)[0...dst.index].reduce(0, :+) + n
       [
-        "#{wirings_label_for(src.object)}_#{src.group}(#{src_bin})",
-        "#{wirings_label_for(dst.object)}_#{dst.group}(#{dst_bin})",
+        [src.object, "#{src.group}(#{src_bin})"],
+        [dst.object, "#{dst.group}(#{dst_bin})"],
       ]
-    end
-
-    def wirings_label_for(object)
-      case
-      when object == circuit          then 'fsm'
-      when functions.include?(object) then "f#{functions.index(object)}"
-      when recoders.include?(object)  then "r#{recoders.index(object)}"
-      end
     end
   end
 end
--- lib/art_decomp/wires_presenter.rb
+++ lib/art_decomp/wires_presenter.rb
@@ -1,8 +1,11 @@
 require 'delegate'
+require 'forwardable'
 require_relative 'wire_presenter'
 
 module ArtDecomp
   class WiresPresenter < SimpleDelegator
+    extend Forwardable
+
     def initialize(wires, circuit:)
       super wires
       @circuit = circuit
@@ -10,11 +13,29 @@ module ArtDecomp
 
     def each(&block)
       flat_map do |wire|
-        WirePresenter.new(wire, circuit: circuit).wirings
+        wirings = WirePresenter.new(wire).wirings
+        wirings.map do |(src_obj, src_lab), (dst_obj, dst_lab)|
+          [
+            "#{wirings_label_for(src_obj)}_#{src_lab}",
+            "#{wirings_label_for(dst_obj)}_#{dst_lab}",
+          ]
+        end
       end.to_h.each(&block)
     end
 
     attr_reader :circuit
     private     :circuit
+
+    private
+
+    delegate %i(functions recoders) => :circuit
+
+    def wirings_label_for(object)
+      case
+      when object == circuit          then 'fsm'
+      when functions.include?(object) then "f#{functions.index(object)}"
+      when recoders.include?(object)  then "r#{recoders.index(object)}"
+      end
+    end
   end
 end
