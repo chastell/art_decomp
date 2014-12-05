31f16af1ebfe7cc1d1e55f6c8b3d3a564aa7d0bc
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2014-12-05 22:34:15 +0100

    factor out PinPresenter#label_for

--- lib/art_decomp/pin_presenter.rb
+++ lib/art_decomp/pin_presenter.rb
@@ -3,10 +3,13 @@ require 'delegate'
 module ArtDecomp
   class PinPresenter < SimpleDelegator
     def labels
-      Array.new(object.binwidths(group)[index]) do |n|
-        bin = object.binwidths(group)[0...index].reduce(0, :+) + n
-        [object, "#{group}(#{bin})"]
-      end
+      Array.new(object.binwidths(group)[index]) { |n| [object, label_for(n)] }
+    end
+
+    private
+
+    def label_for(n)
+      "#{group}(#{object.binwidths(group)[0...index].reduce(0, :+) + n})"
     end
   end
 end
