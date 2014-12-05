9c35203b23a7acc95c0c5b13a55fa4ccc56eeed2
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2014-12-05 21:17:13 +0100

    WiresPresenter#each: simplify

--- lib/art_decomp/wires_presenter.rb
+++ lib/art_decomp/wires_presenter.rb
@@ -13,12 +13,12 @@ module ArtDecomp
 
     def each(&block)
       flat_map do |wire|
-        src = PinPresenter.new(wire.src).wirings
-        dst = PinPresenter.new(wire.dst).wirings
-        src.zip(dst).map do |src_lab, dst_lab|
+        src = PinPresenter.new(wire.src)
+        dst = PinPresenter.new(wire.dst)
+        src.wirings.zip(dst.wirings).map do |src_lab, dst_lab|
           [
-            "#{wirings_label_for(wire.src.object)}_#{src_lab}",
-            "#{wirings_label_for(wire.dst.object)}_#{dst_lab}",
+            "#{wirings_label_for(src.object)}_#{src_lab}",
+            "#{wirings_label_for(dst.object)}_#{dst_lab}",
           ]
         end
       end.each(&block)
