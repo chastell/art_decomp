8dd4fb500bda0fe1ee2dc0736bb1d62ca4044beb
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2014-12-05 21:19:19 +0100

    simplify PinPresenter#labels

--- lib/art_decomp/pin_presenter.rb
+++ lib/art_decomp/pin_presenter.rb
@@ -4,8 +4,7 @@ module ArtDecomp
   class PinPresenter < SimpleDelegator
     def labels
       Array.new(object.binwidths(group)[index]) do |n|
-        bin = object.binwidths(group)[0...index].reduce(0, :+) + n
-        "#{group}(#{bin})"
+        "#{group}(#{object.binwidths(group)[0...index].reduce(0, :+) + n})"
       end
     end
   end
