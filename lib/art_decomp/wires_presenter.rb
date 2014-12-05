d637515048d360b3287f9a5b70a490e03d1ac04e
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2014-12-05 21:50:38 +0100

    factor out WirePresenter again

--- lib/art_decomp/wires_presenter.rb
+++ lib/art_decomp/wires_presenter.rb
@@ -1,5 +1,5 @@
 require 'delegate'
-require_relative 'pin_presenter'
+require_relative 'wire_presenter'
 
 module ArtDecomp
   class WiresPresenter < SimpleDelegator
@@ -9,23 +9,12 @@ module ArtDecomp
     end
 
     def each(&block)
-      flat_map { |wire| wirings_for(wire) }.each(&block)
+      flat_map do |wire|
+        WirePresenter.new(wire, circuit_presenter: circuit_presenter).wirings
+      end.each(&block)
     end
 
     attr_reader :circuit_presenter
     private     :circuit_presenter
-
-    private
-
-    def wirings_for(wire)
-      src = PinPresenter.new(wire.src)
-      dst = PinPresenter.new(wire.dst)
-      src.labels.zip(dst.labels).map do |src_label, dst_label|
-        [
-          "#{circuit_presenter.wirings_label_for(src.object)}_#{src_label}",
-          "#{circuit_presenter.wirings_label_for(dst.object)}_#{dst_label}",
-        ]
-      end
-    end
   end
 end
