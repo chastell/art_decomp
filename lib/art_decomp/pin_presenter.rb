79976cbd4038cefb58de8149d0e30015cd92768e
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2014-12-05 21:19:08 +0100

    inline PinPresenter#label_for

--- lib/art_decomp/pin_presenter.rb
+++ lib/art_decomp/pin_presenter.rb
@@ -3,14 +3,10 @@ require 'delegate'
 module ArtDecomp
   class PinPresenter < SimpleDelegator
     def labels
-      Array.new(object.binwidths(group)[index]) { |n| label_for(n) }
-    end
-
-    private
-
-    def label_for(n)
-      bin = object.binwidths(group)[0...index].reduce(0, :+) + n
-      "#{group}(#{bin})"
+      Array.new(object.binwidths(group)[index]) do |n|
+        bin = object.binwidths(group)[0...index].reduce(0, :+) + n
+        "#{group}(#{bin})"
+      end
     end
   end
 end
