80ef9945c4b5a7fda17d2b3937ec1c08adc855c1
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2014-12-05 21:42:19 +0100

    factor out WiresPresenter#wirings_for

--- lib/art_decomp/wires_presenter.rb
+++ lib/art_decomp/wires_presenter.rb
@@ -9,19 +9,23 @@ module ArtDecomp
     end
 
     def each(&block)
-      flat_map do |wire|
-        src = PinPresenter.new(wire.src)
-        dst = PinPresenter.new(wire.dst)
-        src.labels.zip(dst.labels).map do |src_label, dst_label|
-          [
-            "#{circuit_presenter.wirings_label_for(src.object)}_#{src_label}",
-            "#{circuit_presenter.wirings_label_for(dst.object)}_#{dst_label}",
-          ]
-        end
-      end.each(&block)
+      flat_map { |wire| wirings_for(wire) }.each(&block)
     end
 
     attr_reader :circuit_presenter
     private     :circuit_presenter
+
+    private
+
+    def wirings_for(wire)
+      src = PinPresenter.new(wire.src)
+      dst = PinPresenter.new(wire.dst)
+      src.labels.zip(dst.labels).map do |src_label, dst_label|
+        [
+          "#{circuit_presenter.wirings_label_for(src.object)}_#{src_label}",
+          "#{circuit_presenter.wirings_label_for(dst.object)}_#{dst_label}",
+        ]
+      end
+    end
   end
 end
