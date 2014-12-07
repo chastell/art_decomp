2de2ae765bd544815d87ebe422897aa33cd17264
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2014-12-08 00:57:57 +0100

    actually scope RequiredPutsFilter under ArtDecomp

--- lib/art_decomp/required_puts_filter.rb
+++ lib/art_decomp/required_puts_filter.rb
@@ -1,22 +1,25 @@
-class RequiredPutsFilter
-  def self.required(puts:, required_seps:)
-    puts & new(puts: puts, required_seps: required_seps).required_puts
-  end
 
-  def initialize(puts:, required_seps:)
-    @required_seps = required_seps
-    @sorted_puts = puts.sort_by { |p| (p.seps & required_seps).size }.reverse
-  end
-
-  def required_puts
-    remaining = required_seps
-    sorted_puts.take_while do |put|
-      empty = remaining.empty?
-      remaining -= put.seps
-      not empty
+module ArtDecomp
+  class RequiredPutsFilter
+    def self.required(puts:, required_seps:)
+      puts & new(puts: puts, required_seps: required_seps).required_puts
     end
-  end
 
-  attr_reader :required_seps, :sorted_puts
-  private     :required_seps, :sorted_puts
+    def initialize(puts:, required_seps:)
+      @required_seps = required_seps
+      @sorted_puts = puts.sort_by { |p| (p.seps & required_seps).size }.reverse
+    end
+
+    def required_puts
+      remaining = required_seps
+      sorted_puts.take_while do |put|
+        empty = remaining.empty?
+        remaining -= put.seps
+        not empty
+      end
+    end
+
+    attr_reader :required_seps, :sorted_puts
+    private     :required_seps, :sorted_puts
+  end
 end
