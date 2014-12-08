3ed3fdbdec621eaaf0c7e13371c28db63e0f6af6
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2014-12-08 01:53:21 +0100

    rename PinPresenter#label_for to #label

--- lib/art_decomp/pin_presenter.rb
+++ lib/art_decomp/pin_presenter.rb
@@ -3,12 +3,12 @@ require 'delegate'
 module ArtDecomp
   class PinPresenter < SimpleDelegator
     def labels
-      Array.new(object.binwidths(group)[index]) { |n| [object, label_for(n)] }
+      Array.new(object.binwidths(group)[index]) { |n| [object, label(n)] }
     end
 
     private
 
-    def label_for(n)
+    def label(n)
       "#{group}(#{object.binwidths(group)[0...index].reduce(0, :+) + n})"
     end
   end
