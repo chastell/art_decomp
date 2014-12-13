bcf3d593cd21216eef090f31c67a16addc26e19b
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2014-12-13 03:03:40 +0100

    refactor Puts test a bit

--- test/art_decomp/puts_test.rb
+++ test/art_decomp/puts_test.rb
@@ -4,8 +4,10 @@ require_relative '../../lib/art_decomp/puts'
 
 module ArtDecomp
   describe Puts do
-    let(:a0b1) { Put[a: B[0], b: B[1]] }
-    let(:a1b0) { Put[a: B[1], b: B[0]] }
+    let(:a0b1)   { Put[a: B[0], b: B[1]]          }
+    let(:a0b1c2) { Put[a: B[0], b: B[1], c: B[2]] }
+    let(:a1b0)   { Put[a: B[1], b: B[0]]          }
+    let(:puts)   { Puts.new([a0b1, a1b0])         }
 
     describe '#==' do
       it 'compares Puts according to contents' do
@@ -16,57 +18,57 @@ module ArtDecomp
 
     describe '#&' do
       it 'creates an intersection of Puts' do
-        (Puts.new([a0b1, a1b0]) & Puts.new([a0b1])).must_equal Puts.new([a0b1])
-        (Puts.new([a0b1, a1b0]) & Puts.new([a1b0])).must_equal Puts.new([a1b0])
+        (puts & Puts.new([a0b1])).must_equal Puts.new([a0b1])
+        (puts & Puts.new([a1b0])).must_equal Puts.new([a1b0])
       end
     end
 
     describe '#+' do
       it 'creates a sum of Puts' do
-        (Puts.new([a0b1]) + Puts.new([a1b0])).must_equal Puts.new([a0b1, a1b0])
+        (Puts.new([a0b1]) + Puts.new([a1b0])).must_equal puts
       end
     end
 
     describe '#[]' do
       it 'returns the Put at the given index' do
-        Puts.new([a0b1, a1b0])[0].must_equal a0b1
-        Puts.new([a0b1, a1b0])[1].must_equal a1b0
+        puts[0].must_equal a0b1
+        puts[1].must_equal a1b0
       end
     end
 
     describe '#binwidth' do
       it 'returns the binwidth of all the Puts combined' do
-        Puts.new.binwidth.must_equal 0
-        Puts.new([a0b1]).binwidth.must_equal 1
-        Puts.new([a1b0, Put[a: B[0], b: B[1], c: B[2]]]).binwidth.must_equal 3
+        Puts.new.binwidth.must_equal                 0
+        Puts.new([a0b1]).binwidth.must_equal         1
+        puts.binwidth.must_equal                     2
+        Puts.new([a1b0, a0b1c2]).binwidth.must_equal 3
       end
     end
 
     describe '#each' do
       it 'yields subsequent Puts' do
-        Puts.new([a0b1, a1b0]).each.with_object([]) { |put, ary| ary << put }
-          .must_equal [a0b1, a1b0]
+        puts.each.with_object([]) { |p, ary| ary << p }.must_equal [a0b1, a1b0]
       end
     end
 
     describe '#empty?' do
       it 'is a predicate whether the Puts are empty' do
         assert Puts.new.empty?
-        refute Puts.new([a0b1]).empty?
+        refute puts.empty?
       end
     end
 
     describe '#index' do
       it 'returns the index of the given Put' do
-        Puts.new([a0b1, a1b0]).index(a0b1).must_equal 0
-        Puts.new([a0b1, a1b0]).index(a1b0).must_equal 1
+        puts.index(a0b1).must_equal 0
+        puts.index(a1b0).must_equal 1
       end
     end
 
     describe '#size' do
       it 'returns the number of Puts' do
         Puts.new.size.must_equal 0
-        Puts.new([a0b1, a1b0]).size.must_equal 2
+        puts.size.must_equal     2
       end
     end
   end
