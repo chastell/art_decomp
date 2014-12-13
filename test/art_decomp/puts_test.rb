d8d920bcfbe86eecc804c322498e461e33283a82
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2014-12-13 02:32:54 +0100

    test Puts#&

--- test/art_decomp/puts_test.rb
+++ test/art_decomp/puts_test.rb
@@ -4,10 +4,20 @@ require_relative '../../lib/art_decomp/puts'
 
 module ArtDecomp
   describe Puts do
+    let(:a0b1) { Put[a: B[0], b: B[1]] }
+    let(:a1b0) { Put[a: B[1], b: B[0]] }
+
     describe '#==' do
       it 'compares Puts according to contents' do
-        Puts.new([Put[a: 0, b: 1]]).must_equal Puts.new([Put[a: 0, b: 1]])
-        Puts.new([Put[a: 0, b: 1]]).wont_equal Puts.new([Put[a: 1, b: 0]])
+        Puts.new([a0b1]).must_equal Puts.new([a0b1])
+        Puts.new([a0b1]).wont_equal Puts.new([a1b0])
+      end
+    end
+
+    describe '#&' do
+      it 'creates an intersection of Puts' do
+        (Puts.new([a0b1, a1b0]) & Puts.new([a0b1])).must_equal Puts.new([a0b1])
+        (Puts.new([a0b1, a1b0]) & Puts.new([a1b0])).must_equal Puts.new([a1b0])
       end
     end
   end
