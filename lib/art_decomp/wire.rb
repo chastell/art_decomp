53bddaff3ba3a828f3c59002cb041d906107a68b
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2014-12-19 07:31:08 +0100

    add Wire.from_arrays

--- lib/art_decomp/wire.rb
+++ lib/art_decomp/wire.rb
@@ -1,5 +1,11 @@
+require_relative 'pin'
+
 module ArtDecomp
   Wire = Struct.new(:src, :dst) do
+    def self.from_arrays(source, destination)
+      new(Pin[*source], Pin[*destination])
+    end
+
     def inspect
       "#{self.class}[#{src}, #{dst}]"
     end
--- test/art_decomp/wire_test.rb
+++ test/art_decomp/wire_test.rb
@@ -1,8 +1,18 @@
 require_relative '../test_helper'
+require_relative '../../lib/art_decomp/pin'
 require_relative '../../lib/art_decomp/wire'
 
 module ArtDecomp
   describe Wire do
+    describe '.from_arrays' do
+      it 'constructs the Wire from a minimal Array' do
+        fun_a = fake(:function)
+        fun_b = fake(:function)
+        wire = Wire[Pin[fun_a, :os, 0], Pin[fun_b, :is, 1]]
+        Wire.from_arrays([fun_a, :os, 0], [fun_b, :is, 1]).must_equal wire
+      end
+    end
+
     describe '#inspect' do
       it 'returns self-initialising representation' do
         Wire['src', 'dst'].inspect.must_equal 'ArtDecomp::Wire[src, dst]'
