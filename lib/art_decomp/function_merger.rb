67539cacfcb26b44a3ac9106e483cb68f8553d18
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2014-12-20 00:32:31 +0100

    FunctionMerger.merge: be explicit about the Set of Puts

--- lib/art_decomp/function_merger.rb
+++ lib/art_decomp/function_merger.rb
@@ -5,9 +5,9 @@ require_relative 'puts'
 module ArtDecomp
   module FunctionMerger
     def self.merge(functions)
-      functions.group_by { |fun| fun.is.to_set }.map do |is, funs|
+      functions.group_by { |fun| fun.is.to_set }.map do |is_set, funs|
         os = Puts.new(funs.map(&:os).map(&:puts).flatten.uniq)
-        Function.new(is: Puts.new(is.to_a), os: os)
+        Function.new(is: Puts.new(is_set.to_a), os: os)
       end
     end
   end
