9e1d100c2efb6da7a0c2af43c72114ebc5ba8dd9
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2014-12-21 00:31:49 +0100

    use Puts#uniq to refactor FunctionMerger.merge

--- lib/art_decomp/function_merger.rb
+++ lib/art_decomp/function_merger.rb
@@ -6,7 +6,7 @@ module ArtDecomp
   module FunctionMerger
     def self.merge(functions)
       functions.group_by { |fun| fun.is.to_set }.map do |is_set, funs|
-        os = Puts.new(funs.map(&:os).map(&:puts).flatten.uniq)
+        os = funs.map(&:os).reduce(:+).uniq
         Function.new(is: Puts.new(is_set.to_a), os: os)
       end
     end
