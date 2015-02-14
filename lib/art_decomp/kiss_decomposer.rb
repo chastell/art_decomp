53974399f762034df42ecf1a15fb5196ada5a057
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2015-02-14 22:44:43 +0100

    KISSDecomposer: forward kiss_path and vhdl_path to settings

--- lib/art_decomp/kiss_decomposer.rb
+++ lib/art_decomp/kiss_decomposer.rb
@@ -1,3 +1,4 @@
+require 'forwardable'
 require_relative 'circuit_presenter'
 require_relative 'decomposer'
 require_relative 'kiss_parser'
@@ -5,20 +6,26 @@ require_relative 'settings'
 
 module ArtDecomp
   class KISSDecomposer
+    extend Forwardable
+
     def initialize(args)
       @settings = Settings.new(args)
     end
 
     def decompose(circuit_presenter: CircuitPresenter,
                   decomposer: Decomposer, kiss_parser: KISSParser)
-      circuit = kiss_parser.circuit_for(File.read(settings.kiss_path))
+      circuit = kiss_parser.circuit_for(File.read(kiss_path))
       decomposer.decompositions(circuit).each.with_index do |dc, i|
-        name = "#{File.basename(settings.kiss_path, '.kiss')}_#{i}"
+        name = "#{File.basename(kiss_path, '.kiss')}_#{i}"
         vhdl = circuit_presenter.vhdl_for(dc, name: name)
-        File.write "#{settings.vhdl_path}/#{name}.vhdl", vhdl
+        File.write "#{vhdl_path}/#{name}.vhdl", vhdl
       end
     end
 
     private_attr_reader :settings
+
+    private
+
+    delegate %i(kiss_path vhdl_path) => :settings
   end
 end
