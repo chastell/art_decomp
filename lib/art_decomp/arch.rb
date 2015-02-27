7def3faeda66d75a245e027c825c2d18dee2b339
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2015-02-27 06:31:02 +0100

    Arch: switch to anima

--- lib/art_decomp/arch.rb
+++ lib/art_decomp/arch.rb
@@ -1,5 +1,13 @@
+require 'anima'
+
 module ArtDecomp
-  Arch = Struct.new(:i, :o) do
+  class Arch
+    include Anima.new(:i, :o)
+
+    def self.[](i, o)
+      new(i: i, o: o)
+    end
+
     def <=>(other)
       (i <=> other.i).nonzero? or o <=> other.o
     end
