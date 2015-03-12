0aab1f6de04ba1f81d785acb5507ec2789ec287d
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2015-03-12 05:31:38 +0100

    inline PutPresenter#mapping_for

--- lib/art_decomp/put_presenter.rb
+++ lib/art_decomp/put_presenter.rb
@@ -4,13 +4,13 @@ require_relative 'b'
 module ArtDecomp
   class PutPresenter < SimpleDelegator
     def bin_column
-      column.map { |code| code == :- ? '-' * binwidth : mapping_for(code) }
-    end
-
-    private
-
-    def mapping_for(code)
-      codes.sort.index(code).to_s(2).rjust(binwidth, '0')
+      column.map do |code|
+        if code == :-
+          '-' * binwidth
+        else
+          codes.sort.index(code).to_s(2).rjust(binwidth, '0')
+        end
+      end
     end
   end
 end
