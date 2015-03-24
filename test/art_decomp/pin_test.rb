358bc393548b49f343cb5ad3d80e817657abd7d4
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2015-03-24 06:09:30 +0100

    Pin test: drop direct Put usage

--- test/art_decomp/pin_test.rb
+++ test/art_decomp/pin_test.rb
@@ -1,14 +1,13 @@
 require_relative '../test_helper'
 require_relative '../../lib/art_decomp/function'
 require_relative '../../lib/art_decomp/pin'
-require_relative '../../lib/art_decomp/put'
 require_relative '../../lib/art_decomp/puts'
 
 module ArtDecomp
   describe Pin do
     describe '#inspect' do
       it 'returns self-initialising representation' do
-        ins = Puts.new([Put[%i(a b c)]])
+        ins = Puts.from_columns([[%i(a b c)]])
         function = Function.new(ins: ins, outs: Puts.new)
         Pin[function, :ins, 0].inspect
           .must_equal 'ArtDecomp::Pin' \
