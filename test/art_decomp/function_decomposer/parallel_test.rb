493e1d65c0795e50850b8974fcb9f9ae3e54af0d
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2015-04-02 07:15:03 +0200

    FunctionDecomposer::Parallel test: drop direct Put usage

--- test/art_decomp/function_decomposer/parallel_test.rb
+++ test/art_decomp/function_decomposer/parallel_test.rb
@@ -2,7 +2,6 @@ require_relative '../../test_helper'
 require_relative '../../../lib/art_decomp/circuit'
 require_relative '../../../lib/art_decomp/function'
 require_relative '../../../lib/art_decomp/function_decomposer/parallel'
-require_relative '../../../lib/art_decomp/put'
 require_relative '../../../lib/art_decomp/puts'
 
 module ArtDecomp
@@ -20,15 +19,16 @@ module ArtDecomp
       # 7 | 1 1 1 |  1   1   0
 
       it 'yields decomposed Circuits' do
-        a    = Put[%i(0 0 0 0 1 1 1 1)]
-        b    = Put[%i(0 0 1 1 0 0 1 1)]
-        c    = Put[%i(0 1 0 1 0 1 0 1)]
-        anb  = Put[%i(0 0 0 0 0 0 1 1)]
-        buc  = Put[%i(0 1 1 1 0 1 1 1)]
-        nbuc = Put[%i(1 0 0 0 1 0 0 0)]
-        ab_anb      = Function.new(ins: Puts.new([a,b]), outs: Puts.new([anb]))
-        bc_buc_nbuc = Function.new(ins: Puts.new([b,c]),
-                                   outs: Puts.new([buc, nbuc]))
+        a    = %i(0 0 0 0 1 1 1 1)
+        b    = %i(0 0 1 1 0 0 1 1)
+        c    = %i(0 1 0 1 0 1 0 1)
+        anb  = %i(0 0 0 0 0 0 1 1)
+        buc  = %i(0 1 1 1 0 1 1 1)
+        nbuc = %i(1 0 0 0 1 0 0 0)
+        ab_anb      = Function.new(ins:  Puts.from_columns([a,b]),
+                                   outs: Puts.from_columns([anb]))
+        bc_buc_nbuc = Function.new(ins:  Puts.from_columns([b,c]),
+                                   outs: Puts.from_columns([buc, nbuc]))
         wires = Wires.from_array([
           [[:circuit,    :ins,  0], [ab_anb,      :ins,  0]],
           [[:circuit,    :ins,  1], [ab_anb,      :ins,  1]],
@@ -39,22 +39,23 @@ module ArtDecomp
           [[bc_buc_nbuc, :outs, 1], [:circuit,    :outs, 2]],
         ])
         circuit = Circuit.new(functions: [ab_anb, bc_buc_nbuc],
-                              ins: Puts.new([a, b, c]),
-                              outs: Puts.new([anb, buc, nbuc]),
+                              ins: Puts.from_columns([a, b, c]),
+                              outs: Puts.from_columns([anb, buc, nbuc]),
                               states: Puts.new,
                               next_states: Puts.new,
                               recoders: [],
                               wires: wires)
-        fun = Function.new(ins: Puts.new([a,b,c]),
-                           outs: Puts.new([anb,buc,nbuc]))
+        fun = Function.new(ins: Puts.from_columns([a,b,c]),
+                           outs: Puts.from_columns([anb,buc,nbuc]))
         FunctionDecomposer::Parallel.decompose(fun).to_a.must_equal [circuit]
       end
 
       it 'does not yield if it can’t decompose' do
-        a   = Put[%i(0 0 0 0 1 1 1 1)]
-        b   = Put[%i(0 0 1 1 0 0 1 1)]
-        anb = Put[%i(0 0 0 0 0 0 1 1)]
-        fun = Function.new(ins: Puts.new([a,b]), outs: Puts.new([anb]))
+        a   = %i(0 0 0 0 1 1 1 1)
+        b   = %i(0 0 1 1 0 0 1 1)
+        anb = %i(0 0 0 0 0 0 1 1)
+        fun = Function.new(ins:  Puts.from_columns([a,b]),
+                           outs: Puts.from_columns([anb]))
         FunctionDecomposer::Parallel.decompose(fun).to_a.must_be_empty
       end
     end
