d16f52ab13ba3fb7d75e8161faea0e1c7de527a2
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2015-04-03 00:28:19 +0200

    FunctionMerger test: drop direct Put usage

--- test/art_decomp/function_merger_test.rb
+++ test/art_decomp/function_merger_test.rb
@@ -1,7 +1,6 @@
 require_relative '../test_helper'
 require_relative '../../lib/art_decomp/function'
 require_relative '../../lib/art_decomp/function_merger'
-require_relative '../../lib/art_decomp/put'
 require_relative '../../lib/art_decomp/puts'
 
 module ArtDecomp
@@ -17,19 +16,32 @@ module ArtDecomp
       # 5 | 1 0 1 |  0   1   0
       # 6 | 1 1 0 |  1   1   0
       # 7 | 1 1 1 |  1   1   0
-      let(:a)    { Put[%i(0 0 0 0 1 1 1 1)] }
-      let(:b)    { Put[%i(0 0 1 1 0 0 1 1)] }
-      let(:c)    { Put[%i(0 1 0 1 0 1 0 1)] }
-      let(:anb)  { Put[%i(0 0 0 0 0 0 1 1)] }
-      let(:buc)  { Put[%i(0 1 1 1 0 1 1 1)] }
-      let(:nbuc) { Put[%i(1 0 0 0 1 0 0 0)] }
+      let(:a)    { %i(0 0 0 0 1 1 1 1) }
+      let(:b)    { %i(0 0 1 1 0 0 1 1) }
+      let(:c)    { %i(0 1 0 1 0 1 0 1) }
+      let(:anb)  { %i(0 0 0 0 0 0 1 1) }
+      let(:buc)  { %i(0 1 1 1 0 1 1 1) }
+      let(:nbuc) { %i(1 0 0 0 1 0 0 0) }
 
-      let(:f1) { Function.new(ins: Puts.new([a,b]), outs: Puts.new([anb]))  }
-      let(:f2) { Function.new(ins: Puts.new([b,c]), outs: Puts.new([buc]))  }
-      let(:f3) { Function.new(ins: Puts.new([b,c]), outs: Puts.new([nbuc])) }
-      let(:f4) { Function.new(ins: Puts.new([c,b]), outs: Puts.new([nbuc])) }
+      let(:f1) do
+        Function.new(ins:  Puts.from_columns([a,b]),
+                     outs: Puts.from_columns([anb]))
+      end
+      let(:f2) do
+        Function.new(ins:  Puts.from_columns([b,c]),
+                     outs: Puts.from_columns([buc]))
+      end
+      let(:f3) do
+        Function.new(ins:  Puts.from_columns([b,c]),
+                     outs: Puts.from_columns([nbuc]))
+      end
+      let(:f4) do
+        Function.new(ins:  Puts.from_columns([c,b]),
+                     outs: Puts.from_columns([nbuc]))
+      end
       let(:f23) do
-        Function.new(ins: Puts.new([b,c]), outs: Puts.new([buc,nbuc]))
+        Function.new(ins:  Puts.from_columns([b,c]),
+                     outs: Puts.from_columns([buc,nbuc]))
       end
 
       it 'merges passed Functions according to their inputs' do
