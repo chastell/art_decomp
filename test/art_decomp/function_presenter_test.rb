b93feeec5d6262cde920506acd089635354e6a26
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2015-04-04 01:51:13 +0200

    FunctionPresenter test: drop direct Put usage

--- test/art_decomp/function_presenter_test.rb
+++ test/art_decomp/function_presenter_test.rb
@@ -1,27 +1,22 @@
 require_relative '../test_helper'
 require_relative '../../lib/art_decomp/function'
 require_relative '../../lib/art_decomp/function_presenter'
-require_relative '../../lib/art_decomp/put'
 require_relative '../../lib/art_decomp/puts'
 
 module ArtDecomp
   describe FunctionPresenter do
     describe '#rows' do
       it 'returns binary-encoded row representation of the Function' do
-        ins = Puts.new([
-          Put[%i(0 - 1 - - 1 0 - - -)],
-          Put[%i(- 0 1 - - 0 - 1 - -)],
-          Put[%i(- - - 0 1 - - - 0 1)],
-          Put[%i(HG HG HG HY HY FG FG FG FY FY)],
-        ])
-        outs = Puts.new([
-          Put[%i(0 0 1 0 1 0 1 1 0 1)],
-          Put[%i(0 0 0 0 0 1 1 1 1 1)],
-          Put[%i(0 0 0 1 1 0 0 0 0 0)],
-          Put[%i(1 1 1 1 1 0 0 0 0 0)],
-          Put[%i(0 0 0 0 0 0 0 0 1 1)],
-          Put[%i(HG HG HY HY FG FG FY FY FY HG)],
-        ])
+        ins  = Puts.from_columns([%i(0 - 1 - - 1 0 - - -),
+                                  %i(- 0 1 - - 0 - 1 - -),
+                                  %i(- - - 0 1 - - - 0 1)]) +
+               Puts.from_columns([%i(HG HG HG HY HY FG FG FG FY FY)])
+        outs = Puts.from_columns([%i(0 0 1 0 1 0 1 1 0 1),
+                                  %i(0 0 0 0 0 1 1 1 1 1),
+                                  %i(0 0 0 1 1 0 0 0 0 0),
+                                  %i(1 1 1 1 1 0 0 0 0 0),
+                                  %i(0 0 0 0 0 0 0 0 1 1)]) +
+               Puts.from_columns([%i(HG HG HY HY FG FG FY FY FY HG)])
         function = Function.new(ins: ins, outs: outs)
         function_presenter = FunctionPresenter.new(function)
         function_presenter.rows.must_equal [
