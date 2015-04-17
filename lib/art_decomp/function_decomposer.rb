38c4287cb59884f735e9ab00b4eb38a204ba81a1
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2015-04-17 06:34:34 +0200

    turn FunctionDecomposer.decompose into a module_function

--- lib/art_decomp/function_decomposer.rb
+++ lib/art_decomp/function_decomposer.rb
@@ -3,7 +3,9 @@ require_relative 'function_decomposer/serial'
 
 module ArtDecomp
   module FunctionDecomposer
-    def self.decompose(function, parallel: Parallel, serial: Serial)
+    module_function
+
+    def decompose(function, parallel: Parallel, serial: Serial)
       Enumerator.new do |yielder|
         parallel.decompose(function).each { |circuit| yielder << circuit }
         serial.decompose(function).each   { |circuit| yielder << circuit }
