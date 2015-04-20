8a5e1a375d4584bac54d5e2dd0e07c581817d4a8
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2015-04-21 00:10:10 +0200

    turn CircuitDecomposer.decompose into a module_function

--- lib/art_decomp/circuit_decomposer.rb
+++ lib/art_decomp/circuit_decomposer.rb
@@ -3,8 +3,10 @@ require_relative 'function_decomposer'
 
 module ArtDecomp
   module CircuitDecomposer
-    def self.decompose(circuit, circuit_solder: CircuitSolder.new,
-                                function_decomposer: FunctionDecomposer)
+    module_function
+
+    def decompose(circuit, circuit_solder: CircuitSolder.new,
+                           function_decomposer: FunctionDecomposer)
       function = circuit.largest_function
       Enumerator.new do |yielder|
         function_decomposer.decompose(function).each do |decomposed|
