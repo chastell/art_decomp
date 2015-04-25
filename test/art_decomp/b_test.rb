f501bc6d3db47c4252507898fb6a2b2a42b2763c
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2015-04-25 07:43:53 +0200

    minor refactoring of B.[] test

--- test/art_decomp/b_test.rb
+++ test/art_decomp/b_test.rb
@@ -5,11 +5,15 @@ module ArtDecomp
   describe B do
     describe '.[]' do
       it 'returns an Integer with the given bits set' do
-        B[].must_equal 0b0
-        B[0].must_equal 0b1
-        B[3,7].must_equal 0b10001000
-        B[40].must_equal 2**40
-        B[69,1].must_equal 2**69 + 0b10
+        {
+          B[]     => 0b0,
+          B[0]    => 0b1,
+          B[3,7]  => 0b10001000,
+          B[40]   => 2**40,
+          B[69,1] => 2**69 + 0b10,
+        }.each do |block, number|
+          block.must_equal number
+        end
       end
     end
   end
