128b20bfe5a6a81586c93279a3bdd6db4a15b406
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2015-05-11 22:09:23 +0200

    KISSDecomposer test: don’t get too attached to ‘mc’

--- test/art_decomp/kiss_decomposer_test.rb
+++ test/art_decomp/kiss_decomposer_test.rb
@@ -16,17 +16,17 @@ module ArtDecomp
             decs       = [c1, c2].to_enum
             decomposer = fake(Decomposer, as: :class, decompositions: decs)
             presenter  = fake(CircuitPresenter, as: :class)
-            stub(presenter).vhdl_for(c1, name: 'mc_0') { 'VHDL for mc_0' }
-            stub(presenter).vhdl_for(c2, name: 'mc_1') { 'VHDL for mc_1' }
+            stub(presenter).vhdl_for(c1, name: 'foo_0') { 'VHDL for foo_0' }
+            stub(presenter).vhdl_for(c2, name: 'foo_1') { 'VHDL for foo_1' }
             parser   = fake(KISSParser, as: :class, circuit_for: fake(Circuit))
-            args     = %W(--dir=#{vhdl_path} foo/bar/mc.kiss)
+            args     = %W(--dir=#{vhdl_path} baz/bar/foo.kiss)
             kiss_dec = KISSDecomposer.new(args, circuit_presenter: presenter,
                                                 decomposer: decomposer,
                                                 kiss_parser: parser)
             kiss_dec.decompose
           end
-          File.read("#{vhdl_path}/mc_0.vhdl").must_equal 'VHDL for mc_0'
-          File.read("#{vhdl_path}/mc_1.vhdl").must_equal 'VHDL for mc_1'
+          File.read("#{vhdl_path}/foo_0.vhdl").must_equal 'VHDL for foo_0'
+          File.read("#{vhdl_path}/foo_1.vhdl").must_equal 'VHDL for foo_1'
         end
       end
     end
