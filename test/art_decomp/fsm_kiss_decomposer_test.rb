6fa0aa740b31f69b970097882809504847f24d80
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2015-05-13 23:05:05 +0200

    test FSMKISSDecomposer

---
+++ test/art_decomp/fsm_kiss_decomposer_test.rb
@@ -0,0 +1,34 @@
+require 'tmpdir'
+require_relative '../test_helper'
+require_relative '../../lib/art_decomp/decomposer'
+require_relative '../../lib/art_decomp/fsm'
+require_relative '../../lib/art_decomp/fsm_kiss_decomposer'
+require_relative '../../lib/art_decomp/fsm_kiss_parser'
+require_relative '../../lib/art_decomp/fsm_presenter'
+
+module ArtDecomp
+  describe FSMKISSDecomposer do
+    describe '#decompose' do
+      it 'decomposes the given KISS file into VHDL implementation' do
+        Dir.mktmpdir do |vhdl_path|
+          File.stub(:read, 'some KISS') do
+            c1, c2     = fake(FSM), fake(FSM)
+            decs       = [c1, c2].to_enum
+            decomposer = fake(Decomposer, as: :class, decompositions: decs)
+            presenter  = fake(FSMPresenter, as: :class)
+            stub(presenter).vhdl_for(c1, name: 'foo_0') { 'VHDL for foo_0' }
+            stub(presenter).vhdl_for(c2, name: 'foo_1') { 'VHDL for foo_1' }
+            parser   = fake(FSMKISSParser, as: :class, circuit_for: fake(FSM))
+            args     = %W(--dir=#{vhdl_path} baz/bar/foo.kiss)
+            kiss_dec = FSMKISSDecomposer.new(args, circuit_presenter: presenter,
+                                                   decomposer: decomposer,
+                                                   kiss_parser: parser)
+            kiss_dec.decompose
+          end
+          File.read("#{vhdl_path}/foo_0.vhdl").must_equal 'VHDL for foo_0'
+          File.read("#{vhdl_path}/foo_1.vhdl").must_equal 'VHDL for foo_1'
+        end
+      end
+    end
+  end
+end
