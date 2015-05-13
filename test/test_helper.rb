3b661aee68bab009ae44a0059ce615f62df52803
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2015-05-13 23:26:05 +0200

    test_helper: add a :circ_presenter fake

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,12 +4,17 @@ require 'minitest/pride'
 require 'bogus/minitest/spec'
 require_relative '../lib/art_decomp'
 require_relative '../lib/art_decomp/circuit'
+require_relative '../lib/art_decomp/circuit_presenter'
 require_relative '../lib/art_decomp/fsm'
+require_relative '../lib/art_decomp/fsm_presenter'
 
 Bogus.configure { |config| config.search_modules << ArtDecomp }
 
 Bogus.fakes do
-  fake :circ, class: -> { [ArtDecomp::Circuit, ArtDecomp::FSM] }
+  fake :circ,
+       class: -> { [ArtDecomp::Circuit, ArtDecomp::FSM] }
+  fake :circ_presenter,
+       class: -> { [ArtDecomp::CircuitPresenter, ArtDecomp::FSMPresenter] }
 end
 
 class String
