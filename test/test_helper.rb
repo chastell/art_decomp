65799b281b542971810c67435a8d3e5e06d22bdd
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2015-05-13 23:08:13 +0200

    test_helper: add a :circ fake

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,9 +2,15 @@ require 'minitest/autorun'
 require 'minitest/focus'
 require 'minitest/pride'
 require 'bogus/minitest/spec'
-require 'art_decomp'
+require_relative '../lib/art_decomp'
+require_relative '../lib/art_decomp/circuit'
+require_relative '../lib/art_decomp/fsm'
 
 Bogus.configure { |config| config.search_modules << ArtDecomp }
+
+Bogus.fakes do
+  fake :circ, class: -> { [ArtDecomp::Circuit, ArtDecomp::FSM] }
+end
 
 class String
   def dedent
