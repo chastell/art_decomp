78565cfc513e3ae54120f558e2d8c33b35cb236f
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2015-05-13 23:04:57 +0200

    FSMKISSDecomposer: actually decompose FSMs

--- lib/art_decomp/fsm_kiss_decomposer.rb
+++ lib/art_decomp/fsm_kiss_decomposer.rb
@@ -1,6 +1,13 @@
+require_relative 'decomposer'
+require_relative 'fsm_kiss_parser'
+require_relative 'fsm_presenter'
 require_relative 'kiss_decomposer'
 
 module ArtDecomp
   class FSMKISSDecomposer < KISSDecomposer
+    def initialize(args, circuit_presenter: FSMPresenter,
+                         decomposer: Decomposer, kiss_parser: FSMKISSParser)
+      super
+    end
   end
 end
