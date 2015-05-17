b58f07fd0d336555c6f2f4ced55abeec508b6ee1
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2015-05-17 21:52:13 +0200

    FunctionDecomposer::Parallel: create non-FSM Circuits

--- test/art_decomp/function_decomposer/parallel_test.rb
+++ test/art_decomp/function_decomposer/parallel_test.rb
@@ -1,5 +1,5 @@
 require_relative '../../test_helper'
-require_relative '../../../lib/art_decomp/fsm'
+require_relative '../../../lib/art_decomp/circuit'
 require_relative '../../../lib/art_decomp/function'
 require_relative '../../../lib/art_decomp/function_decomposer/parallel'
 require_relative '../../../lib/art_decomp/puts'
@@ -18,7 +18,7 @@ module ArtDecomp
       # 6 | 1 1 0 |  1   1   0
       # 7 | 1 1 1 |  1   1   0
 
-      it 'yields decomposed FSMs' do
+      it 'yields decomposed Circuits' do
         a    = %i(0 0 0 0 1 1 1 1)
         b    = %i(0 0 1 1 0 0 1 1)
         c    = %i(0 1 0 1 0 1 0 1)
@@ -38,16 +38,13 @@ module ArtDecomp
           [[bc_buc_nbuc, :outs, 0], [:circuit,    :outs, 1]],
           [[bc_buc_nbuc, :outs, 1], [:circuit,    :outs, 2]],
         ])
-        fsm = FSM.new(functions: [ab_anb, bc_buc_nbuc],
-                      ins: Puts.from_columns([a, b, c]),
-                      outs: Puts.from_columns([anb, buc, nbuc]),
-                      states: Puts.new,
-                      next_states: Puts.new,
-                      recoders: [],
-                      wires: wires)
+        circuit = Circuit.new(functions: [ab_anb, bc_buc_nbuc],
+                              ins: Puts.from_columns([a, b, c]),
+                              outs: Puts.from_columns([anb, buc, nbuc]),
+                              wires: wires)
         fun = Function.new(ins: Puts.from_columns([a,b,c]),
                            outs: Puts.from_columns([anb,buc,nbuc]))
-        FunctionDecomposer::Parallel.decompose(fun).to_a.must_equal [fsm]
+        FunctionDecomposer::Parallel.decompose(fun).to_a.must_equal [circuit]
       end
 
       it 'does not yield if it can’t decompose' do
