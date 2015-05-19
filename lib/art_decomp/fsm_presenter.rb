dafdcf10d119040b30c79bd1d8da37714030cbc5
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2015-05-19 05:59:04 +0200

    {Circuit,FSM}Presenter: factor out TEMPLATE_PATH

--- lib/art_decomp/fsm_presenter.rb
+++ lib/art_decomp/fsm_presenter.rb
@@ -3,10 +3,6 @@ require_relative 'circuit_presenter'
 
 module ArtDecomp
   class FSMPresenter < CircuitPresenter
-    def vhdl(name:)
-      super
-      template = File.read('lib/art_decomp/fsm_presenter.vhdl.erb')
-      ERB.new(template, nil, '%').result(binding)
-    end
+    TEMPLATE_PATH = 'lib/art_decomp/fsm_presenter.vhdl.erb'
   end
 end
