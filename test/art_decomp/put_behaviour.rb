01b645ffdc894ff970d382fad2bff621ec9c1912
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2015-05-23 23:00:17 +0200

    Put test: factor out PutBehaviour

---
+++ test/art_decomp/put_behaviour.rb
@@ -0,0 +1,74 @@
+require_relative '../../lib/art_decomp/seps'
+
+module ArtDecomp
+  module PutBehaviour
+    def self.included(spec_class)
+      spec_class.class_eval do
+        describe '.[]' do
+          it 'creates a new put_class with the given column' do
+            put_class[%i(a - b)].must_equal put_class.new(column: %i(a - b))
+            put_class[[]].must_equal put_class.new(column: [])
+          end
+        end
+
+        describe '.new' do
+          it 'builds a put_class from the given column' do
+            put = put_class.new(column: %i(0 1 -), codes: %i(0 1))
+            put.must_equal put_class[%i(0 1 -)]
+          end
+
+          it 'can have the don’t-care and available codes overridden' do
+            put_brackets = put_class[%i(s1 s2 -), codes: %i(s1 s2 s3)]
+            put_new = put_class.new(column: %i(s1 s2 -), codes: %i(s1 s2 s3))
+            put_new.must_equal put_brackets
+          end
+        end
+
+        describe '#==' do
+          it 'compares two put_class instances by value' do
+            assert put_class[%i(a - b)] == put_class[%i(a - b)].dup
+            refute put_class[%i(a - b)] == put_class[%i(b - a)]
+          end
+        end
+
+        describe '#binwidth' do
+          it 'returns the binary width' do
+            put_class[[]].binwidth.must_equal 0
+            put_class[%i(a)].binwidth.must_equal 0
+            put_class[%i(a b)].binwidth.must_equal 1
+            put_class[%i(a b c)].binwidth.must_equal 2
+            put_class[%i(a b c d e f g h)].binwidth.must_equal 3
+            put_class[%i(a b c d e f g h i)].binwidth.must_equal 4
+          end
+        end
+
+        describe '#codes' do
+          it 'returns codes' do
+            put_class[%i(a - b)].codes.must_equal %i(a b)
+          end
+        end
+
+        describe '#inspect' do
+          it 'returns self-initialising representation' do
+            put_class[%i(a - b)].inspect
+              .must_equal put_class.to_s + '[[:a, :-, :b], codes: [:a, :b]]'
+            put_class[%i(1 0)].inspect
+              .must_equal put_class.to_s + '[[:"1", :"0"], codes: [:"0", :"1"]]'
+          end
+        end
+
+        describe '#seps' do
+          it 'returns the put_class’s Seps' do
+            put_class[%i(a - b)].seps.must_equal Seps.new([0b100, 0b000, 0b001])
+          end
+        end
+
+        describe '#size' do
+          it 'returns size' do
+            put_class[%i(a - b)].size.must_equal 2
+          end
+        end
+      end
+    end
+  end
+end
--- test/art_decomp/put_test.rb
+++ test/art_decomp/put_test.rb
@@ -1,70 +1,10 @@
 require_relative '../test_helper'
 require_relative '../../lib/art_decomp/put'
-require_relative '../../lib/art_decomp/seps'
+require_relative 'put_behaviour'
 
 module ArtDecomp
   describe Put do
-    describe '.[]' do
-      it 'creates a new Put with the given column' do
-        Put[%i(a - b)].must_equal Put.new(column: %i(a - b))
-        Put[[]].must_equal Put.new(column: [])
-      end
-    end
-
-    describe '.new' do
-      it 'builds a Put from the given column' do
-        Put.new(column: %i(0 1 -), codes: %i(0 1)).must_equal Put[%i(0 1 -)]
-      end
-
-      it 'can have the don’t-care and available codes overridden' do
-        put = Put[%i(s1 s2 -), codes: %i(s1 s2 s3)]
-        Put.new(column: %i(s1 s2 -), codes: %i(s1 s2 s3)).must_equal put
-      end
-    end
-
-    describe '#==' do
-      it 'compares two Puts by value' do
-        assert Put[%i(a - b)] == Put[%i(a - b)].dup
-        refute Put[%i(a - b)] == Put[%i(b - a)]
-      end
-    end
-
-    describe '#binwidth' do
-      it 'returns the binary width' do
-        Put[[]].binwidth.must_equal 0
-        Put[%i(a)].binwidth.must_equal 0
-        Put[%i(a b)].binwidth.must_equal 1
-        Put[%i(a b c)].binwidth.must_equal 2
-        Put[%i(a b c d e f g h)].binwidth.must_equal 3
-        Put[%i(a b c d e f g h i)].binwidth.must_equal 4
-      end
-    end
-
-    describe '#codes' do
-      it 'returns codes' do
-        Put[%i(a - b)].codes.must_equal %i(a b)
-      end
-    end
-
-    describe '#inspect' do
-      it 'returns self-initialising representation' do
-        Put[%i(a - b)].inspect
-          .must_equal 'ArtDecomp::Put[[:a, :-, :b], codes: [:a, :b]]'
-        Put[%i(1 0)].inspect
-          .must_equal 'ArtDecomp::Put[[:"1", :"0"], codes: [:"0", :"1"]]'
-      end
-    end
-
-    describe '#seps' do
-      it 'returns the Put’s Seps' do
-        Put[%i(a - b)].seps.must_equal Seps.new([0b100, 0b000, 0b001])
-      end
-    end
-
-    describe '#size' do
-      it 'returns size' do
-        Put[%i(a - b)].size.must_equal 2
-      end
-    end
+    include PutBehaviour
+    let(:put_class) { Put }
   end
 end
