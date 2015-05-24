03a139df6155d6c147aac88b720c70841514a22c
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2015-05-24 20:20:27 +0200

    add FSMPresenter#state_binwidth

--- lib/art_decomp/fsm_presenter.rb
+++ lib/art_decomp/fsm_presenter.rb
@@ -4,5 +4,11 @@ require_relative 'circuit_presenter'
 module ArtDecomp
   class FSMPresenter < CircuitPresenter
     TEMPLATE_PATH = 'lib/art_decomp/fsm_presenter.vhdl.erb'
+
+    private
+
+    def state_binwidth
+      states.binwidth
+    end
   end
 end
