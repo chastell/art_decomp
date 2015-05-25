4d6f40949d47d711e533655ae5fbde1717b53fc1
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2015-05-25 23:10:10 +0200

    fold Parallel::Wirer into Wirer

--- lib/art_decomp/function_decomposer/parallel.rb
+++ lib/art_decomp/function_decomposer/parallel.rb
@@ -4,7 +4,7 @@ require_relative '../function'
 require_relative '../function_merger'
 require_relative '../function_simplifier'
 require_relative '../puts'
-require_relative '../wires'
+require_relative '../wirer'
 
 module ArtDecomp
   module FunctionDecomposer
@@ -16,7 +16,9 @@ module ArtDecomp
       def decompositions                       # rubocop:disable Metrics/AbcSize
         Enumerator.new do |yielder|
           unless merged == [function]
-            wires = merged.map { |f| Wirer.new(f, ins, outs).wires }.reduce(:+)
+            wires = merged.map do |fun|
+              Wirer.new(fun, ins: ins, outs: outs).wires
+            end.reduce(:+)
             yielder << Circuit.new(functions: merged, wires: wires)
           end
         end
@@ -31,38 +33,6 @@ module ArtDecomp
           split = outs.map { |o| Function.new(ins: ins, outs: Puts.new([o])) }
           simple = split.map { |fun| FunctionSimplifier.simplify(fun) }
           FunctionMerger.merge(simple)
-        end
-      end
-
-      class Wirer
-        def initialize(function, ins, outs)
-          @function, @ins, @outs = function, ins, outs
-        end
-
-        def wires
-          ins_wires + outs_wires
-        end
-
-        private
-
-        private_attr_reader :function, :ins, :outs
-
-        def ins_wires                          # rubocop:disable Metrics/AbcSize
-          Wires.from_array(function.ins.map.with_index do |put, fi|
-            c_offset = ins[0...ins.index(put)].map(&:binwidth).reduce(0, :+)
-            f_offset = function.ins[0...fi].map(&:binwidth).reduce(0, :+)
-            [[:circuit, :ins, ins.index(put), put.binwidth, c_offset],
-             [function, :ins, fi,             put.binwidth, f_offset]]
-          end)
-        end
-
-        def outs_wires                         # rubocop:disable Metrics/AbcSize
-          Wires.from_array(function.outs.map.with_index do |put, fo|
-            c_offset = outs[0...outs.index(put)].map(&:binwidth).reduce(0, :+)
-            f_offset = function.outs[0...fo].map(&:binwidth).reduce(0, :+)
-            [[function, :outs, fo,              put.binwidth, f_offset],
-             [:circuit, :outs, outs.index(put), put.binwidth, c_offset]]
-          end)
         end
       end
     end
--- lib/art_decomp/wirer.rb
+++ lib/art_decomp/wirer.rb
@@ -14,27 +14,29 @@ module ArtDecomp
 
     private_attr_reader :function, :ins, :outs
 
-    def ins_array
-      ins.map.with_index do |put, n|
-        offset = ins[0...n].map(&:binwidth).reduce(0, :+)
+    def ins_array                              # rubocop:disable Metrics/AbcSize
+      function.ins.map.with_index do |put, n|
+        c_offset = ins[0...ins.index(put)].map(&:binwidth).reduce(0, :+)
+        f_offset = function.ins[0...n].map(&:binwidth).reduce(0, :+)
         source = if put.state?
                    [:circuit, :states, 0, put.binwidth, 0]
                  else
-                   [:circuit, :ins, n, put.binwidth, offset]
+                   [:circuit, :ins, ins.index(put), put.binwidth, c_offset]
                  end
-        [source, [function, :ins, n, put.binwidth, offset]]
+        [source, [function, :ins, n, put.binwidth, f_offset]]
       end.compact
     end
 
-    def outs_array
-      outs.map.with_index do |put, n|
-        offset = outs[0...n].map(&:binwidth).reduce(0, :+)
+    def outs_array                             # rubocop:disable Metrics/AbcSize
+      function.outs.map.with_index do |put, n|
+        c_offset = outs[0...outs.index(put)].map(&:binwidth).reduce(0, :+)
+        f_offset = function.outs[0...n].map(&:binwidth).reduce(0, :+)
         target = if put.state?
                    [:circuit, :next_states, 0, put.binwidth, 0]
                  else
-                   [:circuit, :outs, n, put.binwidth, offset]
+                   [:circuit, :outs, outs.index(put), put.binwidth, c_offset]
                  end
-        [[function, :outs, n, put.binwidth, offset], target]
+        [[function, :outs, n, put.binwidth, f_offset], target]
       end.compact
     end
   end
