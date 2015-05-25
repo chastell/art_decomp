8a0bbd95edab342043b43d5228fee37cb653a6ac
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2015-05-25 22:45:55 +0200

    simplify FSM::Wirer

--- lib/art_decomp/fsm.rb
+++ lib/art_decomp/fsm.rb
@@ -19,44 +19,29 @@ module ArtDecomp
     end
 
     class Wirer < Wirer
-      def wires
-        ins_wires + states_wires + outs_wires + next_states_wires
-      end
-
       private
 
       def ins_wires
         Wires.from_array(ins.map.with_index do |put, n|
           offset = ins[0...n].map(&:binwidth).reduce(0, :+)
-          [[:circuit, :ins, n, put.binwidth, offset],
-           [function, :ins, n, put.binwidth, offset]] unless put.state?
-        end.compact)
-      end
-
-      def next_states_wires
-        offset = 0
-        Wires.from_array(outs.map.with_index do |put, n|
-          offset = outs[0...n].map(&:binwidth).reduce(0, :+)
-          [[function, :outs,        n, put.binwidth, offset],
-           [:circuit, :next_states, 0, put.binwidth, 0]] if put.state?
+          source = if put.state?
+                     [:circuit, :states, 0, put.binwidth, 0]
+                   else
+                     [:circuit, :ins, n, put.binwidth, offset]
+                   end
+          [source, [function, :ins, n, put.binwidth, offset]]
         end.compact)
       end
 
       def outs_wires
-        offset = 0
         Wires.from_array(outs.map.with_index do |put, n|
           offset = outs[0...n].map(&:binwidth).reduce(0, :+)
-          [[function, :outs, n, put.binwidth, offset],
-           [:circuit, :outs, n, put.binwidth, offset]] unless put.state?
-        end.compact)
-      end
-
-      def states_wires
-        offset = 0
-        Wires.from_array(ins.map.with_index do |put, n|
-          offset = ins[0...n].map(&:binwidth).reduce(0, :+)
-          [[:circuit, :states, 0, put.binwidth, 0],
-           [function, :ins,    n, put.binwidth, offset]] if put.state?
+          target = if put.state?
+                     [:circuit, :next_states, 0, put.binwidth, 0]
+                   else
+                     [:circuit, :outs, n, put.binwidth, offset]
+                   end
+          [[function, :outs, n, put.binwidth, offset], target]
         end.compact)
       end
     end
