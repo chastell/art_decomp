1da3efc4092151a860ac71b3c4ddc3b424b0847d
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2015-05-25 22:49:16 +0200

    fold FSM::Wirer into Circuit::Wirer

--- lib/art_decomp/circuit.rb
+++ lib/art_decomp/circuit.rb
@@ -56,17 +56,25 @@ module ArtDecomp
       def ins_wires
         Wires.from_array(ins.map.with_index do |put, n|
           offset = ins[0...n].map(&:binwidth).reduce(0, :+)
-          [[:circuit, :ins, n, put.binwidth, offset],
-           [function, :ins, n, put.binwidth, offset]]
-        end)
+          source = if put.state?
+                     [:circuit, :states, 0, put.binwidth, 0]
+                   else
+                     [:circuit, :ins, n, put.binwidth, offset]
+                   end
+          [source, [function, :ins, n, put.binwidth, offset]]
+        end.compact)
       end
 
       def outs_wires
         Wires.from_array(outs.map.with_index do |put, n|
           offset = outs[0...n].map(&:binwidth).reduce(0, :+)
-          [[function, :outs, n, put.binwidth, offset],
-           [:circuit, :outs, n, put.binwidth, offset]]
-        end)
+          target = if put.state?
+                     [:circuit, :next_states, 0, put.binwidth, 0]
+                   else
+                     [:circuit, :outs, n, put.binwidth, offset]
+                   end
+          [[function, :outs, n, put.binwidth, offset], target]
+        end.compact)
       end
     end
   end
--- lib/art_decomp/fsm.rb
+++ lib/art_decomp/fsm.rb
@@ -1,7 +1,6 @@
 require 'anima'
 require_relative 'circuit'
 require_relative 'function'
-require_relative 'wires'
 
 module ArtDecomp
   class FSM < Circuit
@@ -16,34 +15,6 @@ module ArtDecomp
     # FIXME: figure out a way to inherit this
     def inspect
       "#{self.class}(#{function_archs})"
-    end
-
-    class Wirer < Wirer
-      private
-
-      def ins_wires
-        Wires.from_array(ins.map.with_index do |put, n|
-          offset = ins[0...n].map(&:binwidth).reduce(0, :+)
-          source = if put.state?
-                     [:circuit, :states, 0, put.binwidth, 0]
-                   else
-                     [:circuit, :ins, n, put.binwidth, offset]
-                   end
-          [source, [function, :ins, n, put.binwidth, offset]]
-        end.compact)
-      end
-
-      def outs_wires
-        Wires.from_array(outs.map.with_index do |put, n|
-          offset = outs[0...n].map(&:binwidth).reduce(0, :+)
-          target = if put.state?
-                     [:circuit, :next_states, 0, put.binwidth, 0]
-                   else
-                     [:circuit, :outs, n, put.binwidth, offset]
-                   end
-          [[function, :outs, n, put.binwidth, offset], target]
-        end.compact)
-      end
     end
   end
 end
