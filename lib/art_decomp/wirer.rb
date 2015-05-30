fe7ac1341539b756b353270d45187937df7f4d46
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2015-05-30 10:41:32 +0200

    Wirer#*_array: benefit from Puts#[] returning a Puts

--- lib/art_decomp/wirer.rb
+++ lib/art_decomp/wirer.rb
@@ -17,8 +17,8 @@ module ArtDecomp
     def ins_array                              # rubocop:disable Metrics/AbcSize
       function.ins.map.with_index do |put, n|
         next unless ins.include?(put)
-        c_offset = ins[0...ins.index(put)].map(&:binwidth).reduce(0, :+)
-        f_offset = function.ins[0...n].map(&:binwidth).reduce(0, :+)
+        c_offset = ins[0...ins.index(put)].binwidth
+        f_offset = function.ins[0...n].binwidth
         source = if put.state?
                    [:circuit, :states, 0, put.binwidth, 0]
                  else
@@ -31,8 +31,8 @@ module ArtDecomp
     def outs_array                             # rubocop:disable Metrics/AbcSize
       function.outs.map.with_index do |put, n|
         next unless outs.include?(put)
-        c_offset = outs[0...outs.index(put)].map(&:binwidth).reduce(0, :+)
-        f_offset = function.outs[0...n].map(&:binwidth).reduce(0, :+)
+        c_offset = outs[0...outs.index(put)].binwidth
+        f_offset = function.outs[0...n].binwidth
         target = if put.state?
                    [:circuit, :next_states, 0, put.binwidth, 0]
                  else
