bb1e14d855e841349411e8ade07c185f346f4e4b
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2015-06-06 16:10:10 +0200

    add Puts.from_seps

--- lib/art_decomp/puts.rb
+++ lib/art_decomp/puts.rb
@@ -17,6 +17,10 @@ module ArtDecomp
       new(puts)
     end
 
+    def self.from_seps(allowed:, required:, size:)
+      PutsFromSeps.puts(allowed: allowed, required: required, size: size)
+    end
+
     def initialize(puts = [])
       @puts = puts
     end
@@ -56,6 +60,47 @@ module ArtDecomp
 
     def uniq
       self.class.new(puts.uniq)
+    end
+
+    class PutsFromSeps
+      include Anima.new(:allowed, :required, :size)
+
+      def self.puts(allowed:, required:, size:)
+        new(allowed: allowed, required: required, size: size).puts
+      end
+
+      def puts
+        puts = Puts.new
+        until (required - puts.seps).empty?
+          puts += Puts.new([next_put(required - puts.seps)])
+        end
+        puts
+      end
+
+      private
+
+      def acceptable?(column, row, code)
+        column.map.with_index.all? do |other, i|
+          other == :- or other == code or allowed.separates?(row, i)
+        end
+      end
+
+      def code_for(column, row, missing)
+        forbidden = column.values_at(*missing.seps_of(row)).uniq
+        code = :a
+        code = code.next while forbidden.include?(code)
+        code = :- unless acceptable?(column, row, code)
+        code
+      end
+
+      def next_put(missing)
+        missing.nonempty_by_popcount.permutation do |order|
+          column = Array.new(size, :-)
+          order.each { |row| column[row] = code_for(column, row, missing) }
+          put = Put[column]
+          return put unless (missing & put.seps).empty?
+        end
+      end
     end
   end
 end
