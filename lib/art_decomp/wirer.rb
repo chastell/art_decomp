50aa08f7b9df2bdfb8db5126d1df402ae33f9869
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2015-06-07 18:22:12 +0200

    factor out Wirer#{ins,outs}_{source,destination}

--- lib/art_decomp/wirer.rb
+++ lib/art_decomp/wirer.rb
@@ -14,32 +14,44 @@ module ArtDecomp
 
     private_attr_reader :function, :ins, :outs
 
-    def ins_array                              # rubocop:disable Metrics/AbcSize
+    def ins_array
       function.ins.map.with_index do |put, n|
         next unless ins.include?(put)
-        index = ins.index(put)
-        binwidth = put.binwidth
-        source = if put.state?
-                   [:circuit, :states, 0, binwidth, 0]
-                 else
-                   [:circuit, :ins, index, binwidth, ins[0...index].binwidth]
-                 end
-        [source, [function, :ins, n, binwidth, function.ins[0...n].binwidth]]
+        [ins_source(ins, put), ins_destination(function, put, n)]
       end.compact
     end
 
-    def outs_array                             # rubocop:disable Metrics/AbcSize
+    def ins_destination(function, put, n)
+      [function, :ins, n, put.binwidth, function.ins[0...n].binwidth]
+    end
+
+    def ins_source(ins, put)
+      if put.state?
+        [:circuit, :states, 0, put.binwidth, 0]
+      else
+        offset = ins[0...ins.index(put)].binwidth
+        [:circuit, :ins, ins.index(put), put.binwidth, offset]
+      end
+    end
+
+    def outs_array
       function.outs.map.with_index do |put, n|
         next unless outs.include?(put)
-        index = outs.index(put)
-        binwidth = put.binwidth
-        target = if put.state?
-                   [:circuit, :next_states, 0, binwidth, 0]
-                 else
-                   [:circuit, :outs, index, binwidth, outs[0...index].binwidth]
-                 end
-        [[function, :outs, n, binwidth, function.outs[0...n].binwidth], target]
+        [outs_source(function, put, n), outs_destination(outs, put)]
       end.compact
+    end
+
+    def outs_destination(outs, put)
+      if put.state?
+        [:circuit, :next_states, 0, put.binwidth, 0]
+      else
+        offset = outs[0...outs.index(put)].binwidth
+        [:circuit, :outs, outs.index(put), put.binwidth, offset]
+      end
+    end
+
+    def outs_source(function, put, n)
+      [function, :outs, n, put.binwidth, function.outs[0...n].binwidth]
     end
   end
 end
