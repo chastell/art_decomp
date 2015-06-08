64bc6bb0a91465d474e05b125bf03fbf2e90da13
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2015-06-08 23:08:42 +0200

    inline Wirer#{in,out}_{dst,src}

--- lib/art_decomp/wirer.rb
+++ lib/art_decomp/wirer.rb
@@ -19,27 +19,15 @@ module ArtDecomp
     private_attr_reader :function, :ins, :outs
 
     def ins_array
-      (function.ins & ins).map { |put| [in_src(put), in_dst(put)] }
-    end
-
-    def in_dst(put)
-      [function, :ins, function.ins, put]
-    end
-
-    def in_src(put)
-      [:circuit, :ins, ins, put]
+      (function.ins & ins).map do |put|
+        [[:circuit, :ins, ins, put], [function, :ins, function.ins, put]]
+      end
     end
 
     def outs_array
-      (function.outs & outs).map { |put| [out_src(put), out_dst(put)] }
-    end
-
-    def out_dst(put)
-      [:circuit, :outs, outs, put]
-    end
-
-    def out_src(put)
-      [function, :outs, function.outs, put]
+      (function.outs & outs).map do |put|
+        [[function, :outs, function.outs, put], [:circuit, :outs, outs, put]]
+      end
     end
   end
 end
