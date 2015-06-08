f6c41566af5256c6ac89fc356db8f696653b760a
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2015-06-08 22:44:09 +0200

    define Pin#{binwidth,index,offset} as methods

--- lib/art_decomp/pin.rb
+++ lib/art_decomp/pin.rb
@@ -2,16 +2,27 @@ require 'anima'
 
 module ArtDecomp
   class Pin
-    include Anima.new(:object, :group, :puts, :put, :index, :binwidth, :offset)
+    include Anima.new(:object, :group, :puts, :put)
 
     def self.[](object, group, puts, put, index, binwidth, offset)
-      new(object: object, group: group, puts: puts, put: put, index: index,
-          binwidth: binwidth, offset: offset)
+      new(object: object, group: group, puts: puts, put: put)
+    end
+
+    def binwidth
+      put.binwidth
+    end
+
+    def index
+      puts.index(put)
     end
 
     def inspect
       "#{self.class}" \
       "[#{object.inspect}, #{group.inspect}, #{index}, #{binwidth}, #{offset}]"
+    end
+
+    def offset
+      puts[0...index].binwidth
     end
   end
 end
