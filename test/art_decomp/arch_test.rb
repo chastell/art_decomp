39d96fb5564f9a0e7cc8c1526ec738153ac393b0
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2015-06-24 00:27:26 +0200

    port Arch test to Expectaion syntax

--- test/art_decomp/arch_test.rb
+++ test/art_decomp/arch_test.rb
@@ -6,13 +6,13 @@ module ArtDecomp
     describe '#<=>' do
       it 'makes Archs sortable' do
         a23, a24, a33, a43 = Arch[2,3], Arch[2,4], Arch[3,3], Arch[4,3]
-        [a33, a24, a43, a23].sort.must_equal [a23, a24, a33, a43]
+        _([a33, a24, a43, a23].sort).must_equal [a23, a24, a33, a43]
       end
     end
 
     describe '#inspect' do
       it 'returns self-initialising representation' do
-        Arch[1,2].inspect.must_equal 'ArtDecomp::Arch[1,2]'
+        _(Arch[1,2].inspect).must_equal 'ArtDecomp::Arch[1,2]'
       end
     end
   end
