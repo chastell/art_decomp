18cb49e2e5df65ee9e8a225b9c91db1940c91101
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2015-06-26 05:58:49 +0200

    port B test to Expectaion syntax

--- test/art_decomp/b_test.rb
+++ test/art_decomp/b_test.rb
@@ -5,11 +5,11 @@ module ArtDecomp
   describe B do
     describe '.[]' do
       it 'returns an Integer with the given bits set' do
-        B[].must_equal 0b0
-        B[0].must_equal 0b1
-        B[3,7].must_equal 0b10001000
-        B[40].must_equal 2**40
-        B[69,1].must_equal 2**69 + 0b10
+        _(B[]).must_equal 0b0
+        _(B[0]).must_equal 0b1
+        _(B[3,7]).must_equal 0b10001000
+        _(B[40]).must_equal 2**40
+        _(B[69,1]).must_equal 2**69 + 0b10
       end
     end
   end
