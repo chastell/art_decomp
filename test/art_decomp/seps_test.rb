6fb61e6e2926c9b69543c365a5ddeb0de159e5ba
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2015-07-21 00:37:31 +0200

    port Seps test to Expectation syntax

--- test/art_decomp/seps_test.rb
+++ test/art_decomp/seps_test.rb
@@ -56,40 +56,41 @@ module ArtDecomp                          # rubocop:disable Metrics/ModuleLength
     describe '.from_column' do
       it 'builds a proper matrix' do
         column_to_matrix.each do |column, matrix|
-          Seps.from_column(column).must_equal Seps.new(matrix)
+          _(Seps.from_column(column)).must_equal Seps.new(matrix)
         end
-        Seps.from_column(%i(a)).must_equal Seps.new([])
-        Seps.from_column(%i(a a)).must_equal Seps.new([])
+        _(Seps.from_column(%i(a))).must_equal Seps.new([])
+        _(Seps.from_column(%i(a a))).must_equal Seps.new([])
       end
     end
 
     describe '.new' do
       it 'can take a matrix to start from' do
-        Seps.new([0b100, 0b000, 0b001]).must_equal Seps.from_column(%i(a - b))
+        seps = Seps.new([0b100, 0b000, 0b001])
+        _(seps).must_equal Seps.from_column(%i(a - b))
       end
 
       it 'normalises the matrix' do
-        Seps.from_column(%i(a a)).must_equal Seps.new([])
-        Seps.new([0b000, 0b000, 0b000]).must_equal Seps.new([])
-        Seps.new([0b010, 0b001, 0b000]).must_equal Seps.new([0b10, 0b01])
-        Seps.new([0b000, 0b100, 0b000])
+        _(Seps.from_column(%i(a a))).must_equal Seps.new([])
+        _(Seps.new([0b000, 0b000, 0b000])).must_equal Seps.new([])
+        _(Seps.new([0b010, 0b001, 0b000])).must_equal Seps.new([0b10, 0b01])
+        _(Seps.new([0b000, 0b100, 0b000]))
           .must_equal Seps.new([0b000, 0b100, 0b000])
       end
     end
 
     describe '#&' do
       it 'returns the conjunction of the Seps' do
-        (sep_01_02 & sep_01_12).must_equal sep_01
-        (sep_01_02_03_13 & sep_01_02_12).must_equal sep_01_02
-        (sep_01_02_12 & sep_01_02_03_13).must_equal sep_01_02
+        _(sep_01_02 & sep_01_12).must_equal sep_01
+        _(sep_01_02_03_13 & sep_01_02_12).must_equal sep_01_02
+        _(sep_01_02_12 & sep_01_02_03_13).must_equal sep_01_02
       end
     end
 
     describe '#-' do
       it 'returns the difference of the Seps' do
-        (sep_01_02_03_13 - sep_01_02_12).must_equal sep_03_13
-        (sep_01_02_12 - sep_01_02_03_13).must_equal sep_12
-        (sep_03_13 - sep_03_13).must_be :empty?
+        _(sep_01_02_03_13 - sep_01_02_12).must_equal sep_03_13
+        _(sep_01_02_12 - sep_01_02_03_13).must_equal sep_12
+        _(sep_03_13 - sep_03_13).must_be :empty?
       end
     end
 
@@ -103,44 +104,44 @@ module ArtDecomp                          # rubocop:disable Metrics/ModuleLength
 
     describe '#|' do
       it 'returns the disjunction of the Seps' do
-        (sep_01_12 | sep_01_02).must_equal sep_01_02_12
-        (sep_01_02 | sep_12).must_equal sep_01_02_12
-        (sep_12 | sep_01_02).must_equal sep_01_02_12
+        _(sep_01_12 | sep_01_02).must_equal sep_01_02_12
+        _(sep_01_02 | sep_12).must_equal sep_01_02_12
+        _(sep_12 | sep_01_02).must_equal sep_01_02_12
       end
     end
 
     describe '#count' do
       it 'returns the number of separations' do
-        Seps.from_column([]).count.must_equal 0
-        Seps.from_column(%i(a b)).count.must_equal 1
-        Seps.from_column(%i(a - - b b)).count.must_equal 2
-        Seps.from_column(%i(a b a a c)).count.must_equal 7
-        Seps.from_column(%i(- a - - b)).count.must_equal 1
-        Seps.from_column(%i(a b c d e)).count.must_equal 10
-        Seps.from_column(%i(a b c c)).count.must_equal 5
+        _(Seps.from_column([]).count).must_equal 0
+        _(Seps.from_column(%i(a b)).count).must_equal 1
+        _(Seps.from_column(%i(a - - b b)).count).must_equal 2
+        _(Seps.from_column(%i(a b a a c)).count).must_equal 7
+        _(Seps.from_column(%i(- a - - b)).count).must_equal 1
+        _(Seps.from_column(%i(a b c d e)).count).must_equal 10
+        _(Seps.from_column(%i(a b c c)).count).must_equal 5
       end
     end
 
     describe '#empty?' do
       it 'returns a predicate whether the Seps are empty' do
-        Seps.from_column([]).must_be :empty?
-        Seps.from_column(%i(a a)).must_be :empty?
-        Seps.from_column(%i(a b)).wont_be :empty?
+        assert Seps.from_column([]).empty?
+        assert Seps.from_column(%i(a a)).empty?
+        refute Seps.from_column(%i(a b)).empty?
       end
     end
 
     describe '#inspect' do
       it 'returns a self-initialising representation' do
-        Seps.from_column([]).inspect.must_equal 'ArtDecomp::Seps.new([])'
-        Seps.from_column(%i(a - b)).inspect
+        _(Seps.from_column([]).inspect).must_equal 'ArtDecomp::Seps.new([])'
+        _(Seps.from_column(%i(a - b)).inspect)
           .must_equal 'ArtDecomp::Seps.new([0b100, 0b000, 0b001])'
       end
     end
 
     describe '#nonempty_by_popcount' do
       it 'returns non-empty row numbers in their reverse popcount order' do
-        sep_01_02_03_13.nonempty_by_popcount.must_equal [0, 1, 3, 2]
-        sep_03_13.nonempty_by_popcount.must_equal [3, 0, 1]
+        _(sep_01_02_03_13.nonempty_by_popcount).must_equal [0, 1, 3, 2]
+        _(sep_03_13.nonempty_by_popcount).must_equal [3, 0, 1]
       end
     end
 
@@ -155,16 +156,16 @@ module ArtDecomp                          # rubocop:disable Metrics/ModuleLength
 
     describe '#seps_of' do
       it 'returns an Array of rows separated from the given one' do
-        sep_03_13.seps_of(3).must_equal [0, 1]
-        sep_03_13.seps_of(1).must_equal [3]
-        sep_03_13.seps_of(2).must_equal []
-        sep_03_13.seps_of(4).must_equal []
+        _(sep_03_13.seps_of(3)).must_equal [0, 1]
+        _(sep_03_13.seps_of(1)).must_equal [3]
+        _(sep_03_13.seps_of(2)).must_equal []
+        _(sep_03_13.seps_of(4)).must_equal []
       end
     end
 
     describe '#to_s' do
       it 'returns a readable representation' do
-        sep_01_02.to_s.must_equal <<-end.dedent
+        _(sep_01_02.to_s).must_equal <<-end.dedent
           . x x
           x . .
           x . .
