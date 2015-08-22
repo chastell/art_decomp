96271ad3b88d4061b2dc9e458c64faa101aa0126
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2015-08-23 00:05:08 +0200

    test PutsPresetner#simple

--- test/art_decomp/puts_presenter_test.rb
+++ test/art_decomp/puts_presenter_test.rb
@@ -4,13 +4,17 @@ require_relative '../../lib/art_decomp/puts_presenter'
 
 module ArtDecomp
   describe PutsPresenter do
+    let(:puts_presenter) do
+      puts = Puts.from_columns([%i(0 - 1 - - 1 0 - - -),
+                                %i(- 0 1 - - 0 - 1 - -),
+                                %i(- - - 0 1 - - - 0 1)]) +
+             Puts.from_columns([%i(HG HG HG HY HY FG FG FG FY FY)])
+      PutsPresenter.new(puts)
+    end
+
     describe '#bin_columns' do
       it 'returns binary-encoded column representation of the Puts' do
-        puts = Puts.from_columns([%i(0 - 1 - - 1 0 - - -),
-                                  %i(- 0 1 - - 0 - 1 - -),
-                                  %i(- - - 0 1 - - - 0 1)]) +
-               Puts.from_columns([%i(HG HG HG HY HY FG FG FG FY FY)])
-        _(PutsPresenter.new(puts).bin_columns).must_equal %w(
+        _(puts_presenter.bin_columns).must_equal %w(
           0--10
           -0-10
           11-10
@@ -22,6 +26,23 @@ module ArtDecomp
           --001
           --101
         )
+      end
+    end
+
+    describe '#simple' do
+      it 'returns a simple Array representation of the Puts' do
+        _(puts_presenter.simple).must_equal [
+          '0 - - HG',
+          '- 0 - HG',
+          '1 1 - HG',
+          '- - 0 HY',
+          '- - 1 HY',
+          '1 0 - FG',
+          '0 - - FG',
+          '- 1 - FG',
+          '- - 0 FY',
+          '- - 1 FY',
+        ]
       end
     end
   end
