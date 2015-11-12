46115fb65359f79c0c8286ae865b3b9a6d8226d1
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2015-11-12 20:30:37 +0000

    refactor KISSParser test

--- test/art_decomp/kiss_parser_test.rb
+++ test/art_decomp/kiss_parser_test.rb
@@ -6,35 +6,32 @@ require_relative '../../lib/art_decomp/puts'
 
 module ArtDecomp
   describe KISSParser do
+    let(:kiss) do
+      <<-end.dedent
+        .some comments
+        0- 0--
+        11 1--
+        -0 --0
+      end
+    end
+
+    let(:puts) do
+      {
+        ins:  Puts.from_columns([%i(0 1 -), %i(- 1 0)]),
+        outs: Puts.from_columns([%i(0 1 -), %i(- - -), %i(- - 0)],
+                                codes: %i(0 1)),
+      }
+    end
+
     describe '.circuit_for' do
       it 'returns a Circuit represented by the KISS source' do
-        kiss = <<-end.dedent
-          .some comments
-          0- 0--
-          11 1--
-          -0 --0
-        end
-        ins  = Puts.from_columns([%i(0 1 -), %i(- 1 0)])
-        outs = Puts.from_columns([%i(0 1 -), %i(- - -), %i(- - 0)],
-                                 codes: %i(0 1))
-        circuit = Circuit.from_puts(ins: ins, outs: outs)
-        _(KISSParser.circuit_for(kiss)).must_equal circuit
+        _(KISSParser.circuit_for(kiss)).must_equal Circuit.from_puts(puts)
       end
     end
 
     describe '.function_for' do
       it 'returns a Function represented by the KISS source' do
-        kiss = <<-end.dedent
-          .some comments
-          0- 0--
-          11 1--
-          -0 --0
-        end
-        ins  = Puts.from_columns([%i(0 1 -), %i(- 1 0)])
-        outs = Puts.from_columns([%i(0 1 -), %i(- - -), %i(- - 0)],
-                                 codes: %i(0 1))
-        function = Function.new(ins: ins, outs: outs)
-        _(KISSParser.function_for(kiss)).must_equal function
+        _(KISSParser.function_for(kiss)).must_equal Function.new(puts)
       end
     end
   end
