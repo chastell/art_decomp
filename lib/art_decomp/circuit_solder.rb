bb1e953c412c7ae93948bfdac3656645dd5358ea
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2015-11-12 21:19:06 +0000

    CircuitSolder: initial implementation

--- lib/art_decomp/circuit_solder.rb
+++ lib/art_decomp/circuit_solder.rb
@@ -1,6 +1,67 @@
+require_relative 'circuit'
+require_relative 'pin'
+require_relative 'wire'
+require_relative 'wires'
+
 module ArtDecomp
   class CircuitSolder
     def self.replace(circuit, function, decomposed)
+      new(circuit, function, decomposed).replaced
+    end
+
+    def initialize(circuit, function, decomposed)
+      @circuit    = circuit
+      @function   = function
+      @decomposed = decomposed
+    end
+
+    def replaced
+      Circuit.new(functions: functions, wires: adjusted_wires + new_wires)
+    end
+
+    private
+
+    private_attr_reader :circuit, :function, :decomposed
+
+    def adjusted_wire(wire)
+      case
+      when wire.destination.object == function
+        Wire.new(source: wire.source, destination: destination_pin(wire))
+      when wire.source.object == function
+        Wire.new(source: source_pin(wire), destination: wire.destination)
+      else
+        wire
+      end
+    end
+
+    def adjusted_wires
+      Wires.new(circuit.wires.map { |wire| adjusted_wire(wire) })
+    end
+
+    def destination_pin(wire)
+      found = decomposed.wires.find do |dw|
+        dw.source == Pin[:circuit, :ins, wire.destination.puts,
+                         wire.destination.put]
+      end
+      found.destination
+    end
+
+    def functions
+      circuit.functions - [function] + decomposed.functions
+    end
+
+    def new_wires
+      Wires.new(decomposed.wires.reject do |wire|
+        wire.source.object == :circuit or wire.destination.object == :circuit
+      end)
+    end
+
+    def source_pin(wire)
+      found = decomposed.wires.find do |dw|
+        dw.destination == Pin[:circuit, :outs, wire.source.puts,
+                              wire.source.put]
+      end
+      found.source
     end
   end
 end
---
+++ test/art_decomp/circuit_solder_test.rb
@@ -0,0 +1,85 @@
+require_relative '../test_helper'
+require_relative '../../lib/art_decomp/circuit'
+require_relative '../../lib/art_decomp/circuit_solder'
+require_relative '../../lib/art_decomp/kiss_parser'
+require_relative '../../lib/art_decomp/wires'
+
+module ArtDecomp
+  describe CircuitSolder do
+    describe '.replace' do
+      it 'returns a new Circuit with the decomposed Function replaced' do
+        fun = KISSParser.function_for <<-end.dedent
+          0101-0 0
+          010-00 0
+          -1000- 0
+          01011- 0
+          001--1 0
+          --11-1 0
+          1-110- 0
+          00---0 1
+          -1001- 1
+          1-10-- 1
+        end
+        f0 = KISSParser.function_for <<-end.dedent
+          01- 0
+          0-0 0
+          000 0
+          011 0
+          11- 0
+          110 0
+          001 1
+          10- 1
+        end
+        f1 = KISSParser.function_for <<-end.dedent
+          0100 0
+          0100 0
+          -1-0 0
+          01-0 0
+          001- 0
+          --10 0
+          1--0 0
+          000- 1
+          -1-1 1
+          1--1 1
+        end
+        f_ins = fun.ins
+        composed_wires = Wires.from_array([
+          [[:circuit, :ins, f_ins, f_ins[0]], [fun, :ins, f_ins, f_ins[0]]],
+          [[:circuit, :ins, f_ins, f_ins[1]], [fun, :ins, f_ins, f_ins[1]]],
+          [[:circuit, :ins, f_ins, f_ins[2]], [fun, :ins, f_ins, f_ins[2]]],
+          [[:circuit, :ins, f_ins, f_ins[3]], [fun, :ins, f_ins, f_ins[3]]],
+          [[:circuit, :ins, f_ins, f_ins[4]], [fun, :ins, f_ins, f_ins[4]]],
+          [[:circuit, :ins, f_ins, f_ins[5]], [fun, :ins, f_ins, f_ins[5]]],
+          [[fun, :outs, fun.outs, fun.outs[0]],
+           [:circuit, :outs, fun.outs, fun.outs[0]]],
+        ])
+        decomposed_wires = Wires.from_array([
+          [[:circuit, :ins, f_ins, f_ins[2]], [f0, :ins, f0.ins, f0.ins[0]]],
+          [[:circuit, :ins, f_ins, f_ins[3]], [f0, :ins, f0.ins, f0.ins[1]]],
+          [[:circuit, :ins, f_ins, f_ins[4]], [f0, :ins, f0.ins, f0.ins[2]]],
+          [[:circuit, :ins, f_ins, f_ins[0]], [f1, :ins, f1.ins, f1.ins[0]]],
+          [[:circuit, :ins, f_ins, f_ins[1]], [f1, :ins, f1.ins, f1.ins[1]]],
+          [[:circuit, :ins, f_ins, f_ins[5]], [f1, :ins, f1.ins, f1.ins[2]]],
+          [[f0, :outs, f0.outs, f0.outs[0]], [f1, :ins, f1.ins, f1.ins[3]]],
+          [[f1, :outs, f1.outs, f1.outs[0]],
+           [:circuit, :outs, fun.outs, fun.outs[0]]],
+        ])
+        replaced_wires = Wires.from_array([
+          [[:circuit, :ins, f_ins, f_ins[0]], [f1, :ins, f1.ins, f1.ins[0]]],
+          [[:circuit, :ins, f_ins, f_ins[1]], [f1, :ins, f1.ins, f1.ins[1]]],
+          [[:circuit, :ins, f_ins, f_ins[2]], [f0, :ins, f0.ins, f0.ins[0]]],
+          [[:circuit, :ins, f_ins, f_ins[3]], [f0, :ins, f0.ins, f0.ins[1]]],
+          [[:circuit, :ins, f_ins, f_ins[4]], [f0, :ins, f0.ins, f0.ins[2]]],
+          [[:circuit, :ins, f_ins, f_ins[5]], [f1, :ins, f1.ins, f1.ins[2]]],
+          [[f1, :outs, f1.outs, f1.outs[0]],
+           [:circuit, :outs, fun.outs, fun.outs[0]]],
+          [[f0, :outs, f0.outs, f0.outs[0]], [f1, :ins, f1.ins, f1.ins[3]]],
+        ])
+        circuit    = Circuit.new(functions: [fun], wires: composed_wires)
+        decomposed = Circuit.new(functions: [f0, f1], wires: decomposed_wires)
+        replaced   = Circuit.new(functions: [f0, f1], wires: replaced_wires)
+        _(CircuitSolder.replace(circuit, fun, decomposed)).must_equal replaced
+      end
+    end
+  end
+end
