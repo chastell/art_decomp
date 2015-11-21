bb615df89280738dc45582d2f017583673804ef7
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2015-11-21 21:41:02 +0100

    add {Dst,Src}Pin#group

--- lib/art_decomp/dst_pin.rb
+++ lib/art_decomp/dst_pin.rb
@@ -2,5 +2,8 @@ require_relative 'pin'
 
 module ArtDecomp
   class DstPin < Pin
+    def group
+      object == :circuit ? :outs : :ins
+    end
   end
 end
--- lib/art_decomp/src_pin.rb
+++ lib/art_decomp/src_pin.rb
@@ -2,5 +2,8 @@ require_relative 'pin'
 
 module ArtDecomp
   class SrcPin < Pin
+    def group
+      object == :circuit ? :ins : :outs
+    end
   end
 end
