62c6dd78937395a2630dd9d6addb0ece3fd62d52
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2015-11-21 12:09:14 +0100

    test Pin#binwidth

---
+++ test/art_decomp/pin_test.rb
@@ -0,0 +1,15 @@
+require_relative '../test_helper'
+require_relative '../../lib/art_decomp/pin'
+require_relative '../../lib/art_decomp/put'
+require_relative '../../lib/art_decomp/puts'
+
+module ArtDecomp
+  describe Pin do
+    describe '#bindiwth' do
+      it 'returns the binwidth of the Put' do
+        put = Put[%i(a b c)]
+        _(Pin[:circuit, :ins, Puts.new(put), put].binwidth).must_equal 2
+      end
+    end
+  end
+end
