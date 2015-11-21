e0d670ce06247cde23fbda624e5e2c6512e58234
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2015-11-21 19:27:20 +0100

    refactor PutPresenter#bin_column

--- lib/art_decomp/put_presenter.rb
+++ lib/art_decomp/put_presenter.rb
@@ -3,12 +3,15 @@ require 'delegate'
 module ArtDecomp
   class PutPresenter < SimpleDelegator
     def bin_column
-      column.map do |code|
-        if code == :-
-          '-' * binwidth
-        else
-          codes.sort.index(code).to_s(2).rjust(binwidth, '0')
-        end
+      column.map { |code| mapping[code] }
+    end
+
+    private
+
+    def mapping
+      @mapping ||= begin
+        encodings = Array.new(codes.size) { |i| i.to_s(2).rjust(binwidth, '0') }
+        codes.sort.zip(encodings).to_h.merge(:- => '-' * binwidth)
       end
     end
   end
