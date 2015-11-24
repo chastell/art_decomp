83cdf324e638a96eb592e573036611e281992b6f
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2015-11-25 00:05:55 +0100

    test Pin.[]

--- test/art_decomp/pin_test.rb
+++ test/art_decomp/pin_test.rb
@@ -7,6 +7,13 @@ module ArtDecomp
   describe Pin do
     let(:puts) { Puts.from_columns([%i(a b c), %i(c b a)]) }
 
+    describe '.[]' do
+      it 'allows for a quick creation of a Pin' do
+        pin = Pin.new(object: :circuit, puts: puts, put: puts.first)
+        _(Pin[:circuit, puts, puts.first]).must_equal pin
+      end
+    end
+
     describe '#bindiwth' do
       it 'returns the binwidth of the Put' do
         _(Pin[:circuit, puts, puts.first].binwidth).must_equal 2
