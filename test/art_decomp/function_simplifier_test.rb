8ccea025e92c9a6844381c62f5e93ba9876bbc69
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2015-11-28 00:14:56 +0100

    FunctionSimplifier test: parse functions from KISS

--- test/art_decomp/function_simplifier_test.rb
+++ test/art_decomp/function_simplifier_test.rb
@@ -1,43 +1,69 @@
 require_relative '../test_helper'
-require_relative '../../lib/art_decomp/function'
 require_relative '../../lib/art_decomp/function_simplifier'
-require_relative '../../lib/art_decomp/put'
-require_relative '../../lib/art_decomp/puts'
+require_relative '../../lib/art_decomp/kiss_parser'
 
 module ArtDecomp
   describe FunctionSimplifier do
     describe '.simplify' do
-      #   | a b c | anb buc
-      # --+-------+--------
-      # 0 | 0 0 0 |  0   0
-      # 1 | 0 0 1 |  0   1
-      # 2 | 0 1 0 |  0   1
-      # 3 | 0 1 1 |  0   1
-      # 4 | 1 0 0 |  0   0
-      # 5 | 1 0 1 |  0   1
-      # 6 | 1 1 0 |  1   1
-      # 7 | 1 1 1 |  1   1
-
-      let(:a)   { Put[%i(0 0 0 0 1 1 1 1)] }
-      let(:b)   { Put[%i(0 0 1 1 0 0 1 1)] }
-      let(:c)   { Put[%i(0 1 0 1 0 1 0 1)] }
-      let(:anb) { Put[%i(0 0 0 0 0 0 1 1)] }
-      let(:buc) { Put[%i(0 1 1 1 0 1 1 1)] }
-
       it 'returns the simplest implementation of a Function' do
-        ab_anb  = Function.new(ins: Puts.new([a,b]),   outs: Puts.new([anb]))
-        abc_anb = Function.new(ins: Puts.new([a,b,c]), outs: Puts.new([anb]))
+        abc_anb = KISSParser.function_for <<-end
+          000 0
+          001 0
+          010 0
+          011 0
+          100 0
+          101 0
+          110 1
+          111 1
+        end
+        ab_anb = KISSParser.function_for <<-end
+          00 0
+          00 0
+          01 0
+          01 0
+          10 0
+          10 0
+          11 1
+          11 1
+        end
         _(FunctionSimplifier.simplify(abc_anb)).must_equal ab_anb
       end
 
       it 'maintains put order' do
-        abc_buc = Function.new(ins: Puts.new([a,b,c]), outs: Puts.new([buc]))
-        bc_buc  = Function.new(ins: Puts.new([b,c]),   outs: Puts.new([buc]))
+        abc_buc = KISSParser.function_for <<-end
+          000 0
+          001 1
+          010 1
+          011 1
+          100 0
+          101 1
+          110 1
+          111 1
+        end
+        bc_buc = KISSParser.function_for <<-end
+          00 0
+          01 1
+          10 1
+          11 1
+          00 0
+          01 1
+          10 1
+          11 1
+        end
         _(FunctionSimplifier.simplify(abc_buc)).must_equal bc_buc
       end
 
       it 'does not modify Functions that are the simplest already' do
-        ab_anb = Function.new(ins: Puts.new([a,b]), outs: Puts.new([anb]))
+        ab_anb = KISSParser.function_for <<-end
+          00 0
+          00 0
+          01 0
+          01 0
+          10 0
+          10 0
+          11 1
+          11 1
+        end
         _(FunctionSimplifier.simplify(ab_anb)).must_equal ab_anb
       end
     end
