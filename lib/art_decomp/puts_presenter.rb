9c6664fda5e1125fa7272f5ad49199c4cfd2c7f9
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2015-12-09 06:30:55 +0100

    PutsPresenter#bin_columns: oh so very functional

--- lib/art_decomp/puts_presenter.rb
+++ lib/art_decomp/puts_presenter.rb
@@ -3,7 +3,7 @@ require 'delegate'
 module ArtDecomp
   class PutsPresenter < SimpleDelegator
     def bin_columns
-      map { |put| PutPresenter.new(put).bin_column }.transpose.map(&:join)
+      map(&PutPresenter.method(:new)).map(&:bin_column).transpose.map(&:join)
     end
 
     def simple
