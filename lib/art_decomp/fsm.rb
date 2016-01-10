e49a355cd61771f1c8450aa106191718864d0d80
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2016-01-10 23:26:37 +0100

    FSM is now a Circuit

--- lib/art_decomp/fsm.rb
+++ lib/art_decomp/fsm.rb
@@ -1,18 +1,5 @@
-require 'anima'
-require 'equalizer'
 require_relative 'circuit'
-require_relative 'function'
 
 module ArtDecomp
-  class FSM < Circuit
-    include Anima.new(:functions, :own, :wires)
-    include Equalizer.new(:functions, :own)
-
-    def self.from_function(function)
-      in_wires  = function.ins.map  { |put| { put => put } }
-      out_wires = function.outs.map { |put| { put => put } }
-      wires     = (in_wires + out_wires).reduce({}, :merge)
-      new(functions: [function], own: function, wires: wires)
-    end
-  end
+  FSM = Class.new(Circuit)
 end
