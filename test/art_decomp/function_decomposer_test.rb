5fd38369de2419f9c2856b17ee821a507fe1d566
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2016-01-17 19:13:25 +0100

    FunctionDecomposer.decompose test: better naming

--- test/art_decomp/function_decomposer_test.rb
+++ test/art_decomp/function_decomposer_test.rb
@@ -9,16 +9,18 @@ module ArtDecomp
   describe FunctionDecomposer do
     describe '.decompose' do
       it 'returns parallel and serial decompositions' do
-        function = fake(Function)
-        p1, p2   = fake(Circuit), fake(Circuit)
-        s1, s2   = fake(Circuit), fake(Circuit)
-        parallel = fake(FunctionDecomposer::Parallel, as: :class)
-        serial   = fake(FunctionDecomposer::Serial,   as: :class)
-        stub(parallel).decompose(function) { [p1, p2].to_enum }
-        stub(serial).decompose(function)   { [s1, s2].to_enum }
+        function   = fake(Function)
+        par_decd_a = fake(Circuit)
+        par_decd_b = fake(Circuit)
+        ser_decd_a = fake(Circuit)
+        ser_decd_b = fake(Circuit)
+        parallel   = fake(FunctionDecomposer::Parallel, as: :class)
+        serial     = fake(FunctionDecomposer::Serial,   as: :class)
+        stub(parallel).decompose(function) { [par_decd_a, par_decd_b].to_enum }
+        stub(serial).decompose(function)   { [ser_decd_a, ser_decd_b].to_enum }
         decs = FunctionDecomposer.decompose(function, parallel: parallel,
                                                       serial: serial)
-        _(decs.to_a).must_equal [p1, p2, s1, s2]
+        _(decs.to_a).must_equal [par_decd_a, par_decd_b, ser_decd_a, ser_decd_b]
       end
     end
   end
