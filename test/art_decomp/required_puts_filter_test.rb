e3992ac2c4708c9f922b541ab2bac0f6ccc8bc73
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2016-01-18 01:04:09 +0100

    RequiredPutsFilter.required test: factor out abc

--- test/art_decomp/required_puts_filter_test.rb
+++ test/art_decomp/required_puts_filter_test.rb
@@ -20,23 +20,24 @@ module ArtDecomp
       let(:a)   { Put[%i(0 0 0 0 1 1 1 1)] }
       let(:b)   { Put[%i(0 0 1 1 0 0 1 1)] }
       let(:c)   { Put[%i(0 1 0 1 0 1 0 1)] }
+      let(:abc) { Puts.new([a, b, c])      }
       let(:rpf) { RequiredPutsFilter       }
 
       it 'returns an Array of Puts required by the given Seps' do
         anb  = Put[%i(0 0 0 0 0 0 1 1)]
-        puts = rpf.required(puts: Puts.new([a, b, c]), required_seps: anb.seps)
+        puts = rpf.required(puts: abc, required_seps: anb.seps)
         _(puts).must_equal Puts.new([a, b])
       end
 
       it 'preserves Put order' do
         buc  = Put[%i(0 1 1 1 0 1 1 1)]
-        puts = rpf.required(puts: Puts.new([a, b, c]), required_seps: buc.seps)
+        puts = rpf.required(puts: abc, required_seps: buc.seps)
         _(puts).must_equal Puts.new([b, c])
       end
 
       it 'returns an empty Array if there are no Seps' do
         one  = Put[%i(1 1 1 1 1 1 1 1)]
-        puts = rpf.required(puts: Puts.new([a, b, c]), required_seps: one.seps)
+        puts = rpf.required(puts: abc, required_seps: one.seps)
         _(puts).must_be_empty
       end
     end
