499cccd57600875f66b9c0dc5e135fb57a421783
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2016-02-10 00:38:03 +0100

    RequirePutsFilter: order puts in #call, not .call

--- lib/art_decomp/required_puts_filter.rb
+++ lib/art_decomp/required_puts_filter.rb
@@ -8,12 +8,12 @@ module ArtDecomp
     include Anima.new(:puts, :required_seps)
 
     def self.call(puts:, required_seps:)
-      puts & new(puts: puts, required_seps: required_seps).call
+      new(puts: puts, required_seps: required_seps).call
     end
 
     def call
       remaining = required_seps
-      puts_by_relevant_count.take_while do |put|
+      puts & puts_by_relevant_count.take_while do |put|
         empty = remaining.empty?
         remaining -= put.seps
         not empty
